9ff4471cb6f8cab62080cdb377549ec5f80a3f20
Paul Philippov <themactep@gmail.com>
2023-02-28 17:46:08 -0500

    update driver for rtl8188fu

--- general/package/rtl8188fu-openipc/rtl8188fu-openipc.mk
+++ general/package/rtl8188fu-openipc/rtl8188fu-openipc.mk
@@ -4,20 +4,14 @@
 #
 ################################################################################
 
-RTL8188FU_OPENIPC_VERSION = e2d69b2b0c911c1b59daaf59a582ccdbb73fd97d
-RTL8188FU_OPENIPC_SITE = $(call github,ulli-kroll,rtl8188fu,$(RTL8188FU_OPENIPC_VERSION))
+RTL8188FU_OPENIPC_VERSION = a4dae1f8a417fce807e59936507c4abc1379792d
+RTL8188FU_OPENIPC_SITE = $(call github,themactep,ingenic-rtl8188ftv,$(RTL8188FU_OPENIPC_VERSION))
 RTL8188FU_OPENIPC_LICENSE = GPL-2.0
 
 RTL8188FU_OPENIPC_MODULE_MAKE_OPTS = \
 	CONFIG_RTL8188FU=m \
 	KVER=$(LINUX_VERSION_PROBED) \
 	KSRC=$(LINUX_DIR)
-
-define RTL8188FU_OPENIPC_INSTALL_FIRMWARE
-	$(INSTALL) -D -m 644 $(@D)/firmware/rtl8188fufw.bin \
-		$(TARGET_DIR)/lib/firmware/rtlwifi/rtl8188fufw.bin
-endef
-RTL8188FU_OPENIPC_POST_INSTALL_TARGET_HOOKS += RTL8188FU_OPENIPC_INSTALL_FIRMWARE
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
