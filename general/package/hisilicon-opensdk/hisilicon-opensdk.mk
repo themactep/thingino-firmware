5b72772ac46c0edf7b54e36a92f2eca4628648ea
cronyx <xcronyx@gmail.com>
2023-06-04 21:35:21 +0300

    build imx307 4l from opensdk (#842)

--- general/package/hisilicon-opensdk/hisilicon-opensdk.mk
+++ general/package/hisilicon-opensdk/hisilicon-opensdk.mk
@@ -9,32 +9,30 @@ HISILICON_OPENSDK_SITE = $(call github,openipc,openhisilicon,$(HISILICON_OPENSDK
 HISILICON_OPENSDK_LICENSE = GPL-3.0
 HISILICON_OPENSDK_LICENSE_FILES = LICENSE
 
-# FAMILY = $(shell grep "/board/" $(BR2_CONFIG) | head -1 | cut -d "/" -f 3)
-FAMILY := $(BR2_OPENIPC_SOC_FAMILY)
-
 HISILICON_OPENSDK_MODULE_SUBDIRS = kernel
 HISILICON_OPENSDK_MODULE_MAKE_OPTS = \
 	DISABLE_IST=1 \
 	DISABLE_PM=1 \
 	DISABLE_TDE=1 \
 	DISABLE_VO=1 \
-	CHIPARCH=$(FAMILY)
+	CHIPARCH=$(OPENIPC_FAMILY)
 
-ifeq ($(FAMILY),hi3516ev200)
+ifeq ($(OPENIPC_FAMILY),hi3516ev200)
 	HISILICON_OPENSDK_MODULE_MAKE_OPTS += DISABLE_ISP=1
-	SDK_CODE=0x3516E200
-else ifeq ($(FAMILY),gk7205v200)
-	SDK_CODE=0x7205200
+	HISILICON_OPENSDK_SDK_CODE = 0x3516E200
+else ifeq ($(OPENIPC_FAMILY),gk7205v200)
+	HISILICON_OPENSDK_SDK_CODE = 0x7205200
 endif
 
 # for userspace libraries
 define HISILICON_OPENSDK_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CHIPARCH=$(FAMILY) SDK_CODE=$(SDK_CODE) -C $(@D)/libraries all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CHIPARCH=$(OPENIPC_FAMILY) SDK_CODE=$(HISILICON_OPENSDK_SDK_CODE) -C $(@D)/libraries all
 endef
 
 define HISILICON_OPENSDK_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib/sensors
-	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(FAMILY)/sony_imx335/libsns_imx335.so $(TARGET_DIR)/usr/lib/sensors
+	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_FAMILY)/sony_imx335/libsns_imx335.so $(TARGET_DIR)/usr/lib/sensors
+	$(INSTALL) -D -m 0644 $(@D)/libraries/sensor/$(OPENIPC_FAMILY)/sony_imx307/libsns_imx307.so $(TARGET_DIR)/usr/lib/sensors
 endef
 
 $(eval $(kernel-module))
