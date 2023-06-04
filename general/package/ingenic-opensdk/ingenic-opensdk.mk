8b9e7298678f719201fbd400621c3120c724a65d
cronyx <xcronyx@gmail.com>
2023-06-04 18:25:41 +0300

    t31-t40 drivers moved to opensdk (#841)

--- general/package/ingenic-opensdk/ingenic-opensdk.mk
+++ general/package/ingenic-opensdk/ingenic-opensdk.mk
@@ -4,20 +4,14 @@
 #
 ################################################################################
 
+INGENIC_OPENSDK_VERSION = $(shell git ls-remote https://github.com/OpenIPC/openingenic master | awk '{ print $$1 }')
+INGENIC_OPENSDK_SITE = $(call github,openipc,openingenic,$(INGENIC_OPENSDK_VERSION))
 INGENIC_OPENSDK_LICENSE = GPL-3.0
 INGENIC_OPENSDK_LICENSE_FILES = LICENSE
 
-# FAMILY = $(shell grep "/board/" $(BR2_CONFIG) | head -1 | cut -d "/" -f 3)
-
-
-define INGENIC_OPENSDK_EXTRACT_CMDS
-	cp $(BR2_EXTERNAL_INGENIC_PATH)/../general/package/ingenic-opensdk/src/kernel/* $(@D)/
-endef
-
+INGENIC_OPENSDK_MODULE_SUBDIRS = kernel
 INGENIC_OPENSDK_MODULE_MAKE_OPTS = \
-	CONFIG_GPIO=m \
-	KVER=$(LINUX_VERSION_PROBED) \
-	KSRC=$(LINUX_DIR)
+	SOC=$(OPENIPC_MODEL)
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
