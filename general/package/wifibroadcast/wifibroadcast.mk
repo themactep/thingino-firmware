1e46571aa3eeb054053f4112fe91d61243f471ca
Viktor <35473052+viktorxda@users.noreply.github.com>
2023-06-07 09:13:03 +0200

    General: rename openipc vendor variables

--- general/package/wifibroadcast/wifibroadcast.mk
+++ general/package/wifibroadcast/wifibroadcast.mk
@@ -7,6 +7,11 @@ WIFIBROADCAST_VERSION = 23.01
 WIFIBROADCAST_SITE = $(call github,svpcom,wfb-ng,wfb-ng-$(WIFIBROADCAST_VERSION))
 WIFIBROADCAST_LICENSE = GPL-2.0
 
+WIFIBROADCAST_FPATH = air
+ifeq ($(OPENIPC_SOC_FAMILY),hi3536dv100)
+	WIFIBROADCAST_FPATH = gs
+endif
+
 WIFIBROADCAST_DEPENDENCIES += libpcap libsodium iw
 
 WIFIBROADCAST_MAKE_OPTS = \
@@ -23,19 +28,18 @@ define WIFIBROADCAST_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/wfb_keygen $(TARGET_DIR)/usr/bin
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	cp $(BR2_EXTERNAL)/../general/package/wifibroadcast/files/wfb.conf $(TARGET_DIR)/etc
-
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
-	cp $(BR2_EXTERNAL)/../general/package/wifibroadcast/files/S98wfb $(TARGET_DIR)/etc/init.d
+	cp $(WIFIBROADCAST_PKGDIR)/files/$(WIFIBROADCAST_FPATH)/wfb.conf $(TARGET_DIR)/etc
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	cp $(BR2_EXTERNAL)/../general/package/wifibroadcast/files/telemetry $(TARGET_DIR)/usr/bin
+	cp $(WIFIBROADCAST_PKGDIR)/files/$(WIFIBROADCAST_FPATH)/wifibroadcast $(TARGET_DIR)/usr/bin
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	cp $(BR2_EXTERNAL)/../general/package/wifibroadcast/files/tweaksys $(TARGET_DIR)/usr/bin
+	cp $(WIFIBROADCAST_PKGDIR)/files/setmcs $(TARGET_DIR)/usr/bin
 
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	cp $(BR2_EXTERNAL)/../general/package/wifibroadcast/files/telemetry.conf $(TARGET_DIR)/etc
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware/ath9k_htc
+	cp $(WIFIBROADCAST_PKGDIR)/files/htc_9271.fw.1 $(TARGET_DIR)/lib/firmware/ath9k_htc
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware/ath9k_htc
+	cp $(WIFIBROADCAST_PKGDIR)/files/htc_9271.fw.3 $(TARGET_DIR)/lib/firmware/ath9k_htc
 endef
 
 $(eval $(generic-package))
