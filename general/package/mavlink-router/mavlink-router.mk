a642af3371c405bbd78f1c7a32eed8b0337babd8
Paul Philippov <themactep@gmail.com>
2023-10-26 19:57:13 -0400

    merge changes from upstream

--- general/package/mavlink-router/mavlink-router.mk
+++ general/package/mavlink-router/mavlink-router.mk
@@ -4,21 +4,34 @@
 #
 ################################################################################
 
+MAVLINK_ROUTER_SITE_METHOD = git
+MAVLINK_ROUTER_SITE = https://github.com/mavlink-router/mavlink-router
 MAVLINK_ROUTER_VERSION = 18c06da77563db9ba1b7c4173ff5cae1646b383e
-MAVLINK_ROUTER_SITE = $(call github,mavlink-router,mavlink-router,$(MAVLINK_ROUTER_VERSION))
 MAVLINK_ROUTER_LICENSE = ASF-2.0
 
 MAVLINK_ROUTER_DEPENDENCIES += host-meson host-pkgconf
+MAVLINK_ROUTER_GIT_SUBMODULES = YES
+
+MAVLINK_ROUTER_UNIT = drone
+ifeq ($(OPENIPC_SOC_FAMILY),hi3536dv100)
+	MAVLINK_ROUTER_UNIT = gs
+endif
+
+ifeq ($(BR2_TOOLCHAIN_USES_GLIBC),y)
+MAVLINK_ROUTER_LIBC = glibc
+else
+MAVLINK_ROUTER_LIBC = musl
+endif
+
+ifeq ($(BR2_ARM_EABIHF),y)
+MAVLINK_ROUTER_ABI = -hf
+endif
 
 MAVLINK_ROUTER_CONF_OPTS += \
 	--buildtype=release \
-	--cross-file $(MAVLINK_ROUTER_PKGDIR)/files/meson-cross-$(ARCH)-openipc.ini
+	--cross-file $(MAVLINK_ROUTER_PKGDIR)/files/meson-cross-$(ARCH)-$(MAVLINK_ROUTER_LIBC)$(MAVLINK_ROUTER_ABI)-openipc.ini
 
 define MAVLINK_ROUTER_CONFIGURE_CMDS
-	rm -rf $(@D)/build
-	mkdir -p $(@D)/build
-	cp -f $(MAVLINK_ROUTER_PKGDIR)/files/prepare.sh $(@D)/prepare.sh
-	cd $(@D) ; sh prepare.sh
 	$(TARGET_MAKE_ENV) meson setup $(MAVLINK_ROUTER_CONF_OPTS) $(@D) $(@D)/build
 endef
 
@@ -30,7 +43,7 @@ define MAVLINK_ROUTER_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) ninja -C $(@D)/build install
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	cp $(MAVLINK_ROUTER_PKGDIR)/files/mavlink.conf $(TARGET_DIR)/etc/mavlink.conf
+	cp $(MAVLINK_ROUTER_PKGDIR)/files/mavlink_$(MAVLINK_ROUTER_UNIT).conf $(TARGET_DIR)/etc/mavlink.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	cp $(MAVLINK_ROUTER_PKGDIR)/files/S97mavlink $(TARGET_DIR)/etc/init.d
