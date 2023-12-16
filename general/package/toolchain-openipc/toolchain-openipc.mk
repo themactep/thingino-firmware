618289dd46a602647f21f6022a8d220ff5a06cb4
Paul Philippov <themactep@gmail.com>
2023-12-15 22:35:19 -0500

    add missing separators

--- general/package/toolchain-openipc/toolchain-openipc.mk
+++ general/package/toolchain-openipc/toolchain-openipc.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 
-ifeq($(BR2_PACKAGE_TOOLCHAIN_OPENIPC),y)
+ifeq ($(BR2_PACKAGE_TOOLCHAIN_OPENIPC),y)
 BR2_TOOLCHAIN_EXTERNAL_URL="https://github.com/openipc/firmware/releases/download/$(OPENIPC_TOOLCHAIN).tgz"
 
-ifeq($(BR2_arm),y)
+ifeq ($(BR2_arm),y)
 BR2_TOOLCHAIN_EXTERNAL_CUSTOM_PREFIX="arm-openipc-linux-musleabi"
 
-else ifeq($(BR2_mipsel),y)
-ifeq($(BR2_DEFAULT_KERNEL_VERSION),"3.10.14")
+else ifeq ($(BR2_mipsel),y)
+ifeq ($(BR2_DEFAULT_KERNEL_VERSION),"3.10.14")
 BR2_TOOLCHAIN_EXTERNAL_HEADERS_3_10=y
 endif
 
