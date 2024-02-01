328f67105b0ec762bf0f71cc99f79ba2a7fe8a45
Paul Philippov <themactep@gmail.com>
2024-02-01 11:18:01 -0500

    pin majestic

--- package/majestic/majestic.mk
+++ package/majestic/majestic.mk
@@ -4,14 +4,10 @@
 #
 ################################################################################
 
-MAJESTIC_SITE = https://openipc.s3-eu-west-1.amazonaws.com
-MAJESTIC_SOURCE = majestic.$(MAJESTIC_FAMILY).$(MAJESTIC_RELEASE).master.tar.bz2
-
 MAJESTIC_LICENSE = PROPRIETARY
 MAJESTIC_LICENSE_FILES = LICENSE
 
 $(eval MAJESTIC_FAMILY = $(patsubst "%",%,$(SOC_FAMILY)))
-
 ifeq ($(MAJESTIC_FAMILY),t10)
 	MAJESTIC_FAMILY= t21
 endif
@@ -25,24 +21,23 @@ MAJESTIC_DEPENDENCIES = \
 	mbedtls \
 	mxml \
 	zlib
-#	lame
 
 define MAJESTIC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/majestic.yaml
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(@D)/majestic.full
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(MAJESTIC_PKGDIR)/files/majestic.yaml
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(MAJESTIC_PKGDIR)/files/S95majestic
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/majestic
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(MAJESTIC_PKGDIR)/files/$(MAJESTIC_RELEASE)/$(MAJESTIC_FAMILY)/majestic
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/fonts/truetype
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/fonts/truetype $(MAJESTIC_PKGDIR)/files/UbuntuMono-Regular.ttf
 
 	# Majestic is compiled for older libmbedtls
 	[ ! -f "$(TARGET_DIR)/usr/lib/libmbedtls.so.13" ] && ln -srfv $(TARGET_DIR)/usr/lib/libmbedtls.so.14 $(TARGET_DIR)/usr/lib/libmbedtls.so.13
 	[ ! -f "$(TARGET_DIR)/usr/lib/libmbedcrypto.so.6" ] && ln -srfv $(TARGET_DIR)/usr/lib/libmbedcrypto.so.7 $(TARGET_DIR)/usr/lib/libmbedcrypto.so.6
-
-	rm -rf $(MAJESTIC_DL_DIR)
 endef
 
 $(eval $(generic-package))
