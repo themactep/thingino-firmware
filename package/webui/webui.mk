61d1cb9b15733a35f07ed163602d10febe7655dd
Paul Philippov <themactep@gmail.com>
2024-02-03 05:26:45 -0500

    address reshuffled webui repo structure

--- package/webui/webui.mk
+++ package/webui/webui.mk
@@ -12,18 +12,15 @@ WEBUI_LICENSE = MIT
 WEBUI_LICENSE_FILES = LICENSE
 
 define WEBUI_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/var
+	cp -rv $(@D)/www $(TARGET_DIR)/var
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
 	cp $(WEBUI_PKGDIR)/files/httpd.conf $(TARGET_DIR)/etc
 
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
-	cp $(WEBUI_PKGDIR)/files/S50httpd $(TARGET_DIR)/etc/init.d
-	cp -rv $(@D)/files/etc/init.d/* $(TARGET_DIR)/etc/init.d
-
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr
-	cp -rv $(@D)/files/usr/sbin $(TARGET_DIR)/usr
-
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/var
-	cp -rv $(@D)/files/var/www $(TARGET_DIR)/var
+#	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+#	cp $(WEBUI_PKGDIR)/files/S50httpd $(TARGET_DIR)/etc/init.d
+#	cp -rv $(@D)/files/etc/init.d/* $(TARGET_DIR)/etc/init.d
 endef
 
 $(eval $(generic-package))
