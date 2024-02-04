a5d601ad5225de706fdafdb1c88cd5452d18bd3f
Paul Philippov <themactep@gmail.com>
2024-02-04 13:45:38 -0500

    clean up

--- package/ingenic-osdrv-t31/ingenic-osdrv-t31.mk
+++ package/ingenic-osdrv-t31/ingenic-osdrv-t31.mk
@@ -14,10 +14,6 @@ define INGENIC_OSDRV_T31_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T31_PKGDIR)/files/sensor/$(BR2_SENSOR_MODEL).yaml
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T31_PKGDIR)/files/sensor/params/$(BR2_SENSOR_MODEL)-t31.bin
 
-	# Modules for T31 are built from the openingenic repository sources, so we don't need to copy them.
-	# $(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.14__isvp_swan_1.0__/ingenic
-	# $(INSTALL) -m 644 -t $(TARGET_DIR)/lib/modules/3.10.14__isvp_swan_1.0__/ingenic $(INGENIC_OSDRV_T31_PKGDIR)/files/kmod/*.ko
-
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(INGENIC_OSDRV_T31_PKGDIR)/files/script/load*
 
