ae370bebdb84c88f99b2b7c9bde08fc4d4cdcec0
Paul Philippov <themactep@gmail.com>
2024-02-05 18:02:01 -0500

    make symlinks to imitate older libs for majestic, conditionally

--- package/majestic/majestic.mk
+++ package/majestic/majestic.mk
@@ -14,7 +14,7 @@ endif
 
 MAJESTIC_RELEASE = lite
 
-ifeq ($(BR2_SOC_INGENIC_T20),y)
+ifeq ($(BR2_SOC_INGENIC_T20), y)
 MAJESTIC_DEPENDENCIES += \
 	libevent-openipc \
 	libogg-openipc \
@@ -22,7 +22,7 @@ MAJESTIC_DEPENDENCIES += \
 	opus-openipc
 endif
 
-ifneq ($(BR2_SOC_INGENIC_T20),y)
+ifneq ($(BR2_SOC_INGENIC_T20), y)
 MAJESTIC_DEPENDENCIES += \
 	libevent \
 	libogg \
@@ -48,9 +48,10 @@ define MAJESTIC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/fonts/truetype
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/fonts/truetype $(MAJESTIC_PKGDIR)/files/UbuntuMono-Regular.ttf
 
-	# Majestic is compiled for older libmbedtls
-	#[ ! -f "$(TARGET_DIR)/usr/lib/libmbedtls.so.13" ] && ln -srfv $(TARGET_DIR)/usr/lib/libmbedtls.so.14 $(TARGET_DIR)/usr/lib/libmbedtls.so.13
-	#[ ! -f "$(TARGET_DIR)/usr/lib/libmbedcrypto.so.6" ] && ln -srfv $(TARGET_DIR)/usr/lib/libmbedcrypto.so.7 $(TARGET_DIR)/usr/lib/libmbedcrypto.so.6
+	if ! grep -q "^BR2_SOC_INGENIC_T20=y" $(BR2_CONFIG); then \
+		ln -srfv $(TARGET_DIR)/usr/lib/libmbedtls.so.14 $(TARGET_DIR)/usr/lib/libmbedtls.so.13; \
+		ln -srfv $(TARGET_DIR)/usr/lib/libmbedcrypto.so.7 $(TARGET_DIR)/usr/lib/libmbedcrypto.so.6; \
+	fi
 endef
 
 $(eval $(generic-package))
