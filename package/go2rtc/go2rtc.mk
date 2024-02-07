8632ae69d6754d90ab42d5006aacc0e8f6b91475
gtxaspec <gtxent@gmail.com>
2024-02-07 22:10:36 +0000

    go2rtc makefile: fix build path

--- package/go2rtc/go2rtc.mk
+++ package/go2rtc/go2rtc.mk
@@ -18,7 +18,7 @@ GO2RTC_GO_ENV = GOARCH=mipsle
 GO2RTC_LDFLAGS = -s -w
 
 define GO2RTC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/go2rtc $(TARGET_DIR)/usr/bin/go2rtc
+	$(INSTALL) -D -m 0755 $(@D)/bin/go2rtc $(TARGET_DIR)/usr/bin/go2rtc
 	$(HOST_DIR)/bin/upx --best --lzma $(TARGET_DIR)/usr/bin/go2rtc
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(GO2RTC_PKGDIR)/files/go2rtc.yaml
 endef
