9480f0f5fa6251cec388fa3e938b54eb70df11fa
Paul Philippov <themactep@gmail.com>
2024-02-07 19:55:30 -0500

    update information in os-release

--- scripts/rootfs_script.sh
+++ scripts/rootfs_script.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-DATE=$(date +%y.%m.%d)
+#NAME=Thingino
+#VERSION=1
+#ID=thingino
+#VERSION_ID=1
+#PRETTY_NAME="Thingino 1 (Ciao)"
+#ANSI_COLOR="1;34"
+#CPE_NAME="cpe:/o:thinginoproject:thingino:1"
+#HOME_URL="https://thingino.com/"
+#SUPPORT_URL="https://help.thingino.com/"
+#BUG_REPORT_URL="https://issues.thingino.com/"
+#PRIVACY_POLICY_URL="https://policy.thingino.com/"
+#BUILD_ID=
+#VARIANT="Ingenic T31 Edition"
+#VARIANT_ID="ingenic_t31"
+
 FILE=${TARGET_DIR}/usr/lib/os-release
 
-echo OPENIPC_VERSION=${DATE:0:1}.${DATE:1} >> ${FILE}
-date +GITHUB_VERSION="\"${GIT_BRANCH-local}+${GIT_HASH-build}, %Y-%m-%d"\" >> ${FILE}
+GIT_BRANCH=$(git branch | grep ^* | awk '{print $2}')
+GIT_HASH=$(git show -s --format=%H)
+GIT_TIME=$(git show -s --format=%ci)
+
+echo "GITHUB_VERSION=\"${GIT_BRANCH}+${GIT_HASH}, ${GIT_TIME}\"" >> ${FILE}
 date +TIME_STAMP=%s >> ${FILE}
 
-CONF="INGENIC_OSDRV_T30=y|LIBV4L=y|MAVLINK_ROUTER=y|WIFIBROADCAST=y|WEBRTC_AUDIO_PROCESSING=y"
-if ! grep -q "USES_GLIBC" ${BR2_CONFIG} && ! grep -qP ${CONF} ${BR2_CONFIG}; then
-  rm -f ${TARGET_DIR}/usr/lib/libstdc++*
-fi
+#CONF="INGENIC_OSDRV_T30=y|LIBV4L=y|MAVLINK_ROUTER=y|WIFIBROADCAST=y|WEBRTC_AUDIO_PROCESSING=y"
+#if ! grep -q "USES_GLIBC" ${BR2_CONFIG} && ! grep -qP ${CONF} ${BR2_CONFIG}; then
+#  rm -f ${TARGET_DIR}/usr/lib/libstdc++*
+#fi
 
-if grep -q "USES_MUSL" ${BR2_CONFIG}; then
-  LIST=${BR2_EXTERNAL}/scripts/excludes/${SOC_MODEL}.list
-  test -e ${LIST} && xargs -a ${LIST} -I % rm -rf ${TARGET_DIR}/%
-
-  ln -sf libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
-  ln -sf ../../lib/libc.so ${TARGET_DIR}/usr/bin/ldd
-fi
+#if grep -q "USES_MUSL" ${BR2_CONFIG}; then
+#  LIST=${BR2_EXTERNAL}/scripts/excludes/${SOC_MODEL}.list
+#  test -e ${LIST} && xargs -a ${LIST} -I % rm -rf ${TARGET_DIR}/%
+#
+#  ln -sf libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
+#  ln -sf ../../lib/libc.so ${TARGET_DIR}/usr/bin/ldd
+#fi
