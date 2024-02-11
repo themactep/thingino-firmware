c6d38fbcce74d6b00bf11e9f7db5eeccdfa5e550
Paul Philippov <themactep@gmail.com>
2024-02-10 20:40:48 -0500

    clean up; refactor repetitive data to variables

--- package/ingenic-osdrv-t40/ingenic-osdrv-t40.mk
+++ package/ingenic-osdrv-t40/ingenic-osdrv-t40.mk
@@ -11,20 +11,8 @@ INGENIC_OSDRV_T40_LICENSE_FILES = LICENSE
 
 define INGENIC_OSDRV_T40_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/*.yaml
-
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/gc4653-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/imx307-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/imx415-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/jxk04-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/sc3335-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/sc500ai-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/sc5235-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/sc8238-t40.bin
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/sc830ai-t40.bin
-
-        # Modules for T40 are built from the openingenic repository sources, so we don't need to copy them.
-	#$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/4.4.94/ingenic
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/$(BR2_SENSOR_MODEL).yaml
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T40_PKGDIR)/files/sensor/params/$(BR2_SENSOR_MODEL)-t40.bin
 	echo $(BR2_SENSOR_MODEL) >$(TARGET_DIR)/etc/sensor/model
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
