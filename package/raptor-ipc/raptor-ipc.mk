a6539cc78c8eb59488ce5d18a65e9bac0cbdde47
gtxaspec <gtxent@gmail.com>
2024-02-14 00:55:27 -0800

    update raptor makefile

--- package/raptor-ipc/raptor-ipc.mk
+++ package/raptor-ipc/raptor-ipc.mk
@@ -1,12 +1,15 @@
 RAPTOR_IPC_SITE_METHOD = git
 RAPTOR_IPC_SITE = https://github.com/gtxaspec/raptor
-RAPTOR_IPC_VERSION = $(shell git ls-remote $(RAPTOR_IPC_SITE) HEAD | head -1 | cut -f1)
+RAPTOR_IPC_VERSION = $(shell git ls-remote $(RAPTOR_IPC_SITE) rvd-dev | head -1 | cut -f1)
 RAPTOR_IPC_LICENSE = GPL-3.0
 RAPTOR_IPC_LICENSE_FILES = COPYING
 
+define RAPTOR_IPC_BUILD_CMDS
+	$(MAKE) $(RAPTOR_IPC_MAKE_OPTS) -C $(@D) TARGET=$(SOC_FAMILY)
+endef
+
 define RAPTOR_IPC_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(RAPTOR_IPC_PKGDIR)/files/$(SOC_FAMILY)/raptor
+	$(INSTALL) -D -m 0755 $(@D)/raptor $(TARGET_DIR)/usr/bin/raptor
 endef
 
 $(eval $(generic-package))
