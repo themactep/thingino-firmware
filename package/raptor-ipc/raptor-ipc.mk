81fd2acfe87c242d4bda88afdb3636689e37ca5f
Paul Philippov <themactep@gmail.com>
2024-02-14 23:39:20 -0500

    send sensor and soc to raptor

--- package/raptor-ipc/raptor-ipc.mk
+++ package/raptor-ipc/raptor-ipc.mk
@@ -1,15 +1,20 @@
 RAPTOR_IPC_SITE_METHOD = git
 RAPTOR_IPC_SITE = https://github.com/gtxaspec/raptor
 RAPTOR_IPC_VERSION = $(shell git ls-remote $(RAPTOR_IPC_SITE) rvd-dev | head -1 | cut -f1)
+
 RAPTOR_IPC_LICENSE = GPL-3.0
 RAPTOR_IPC_LICENSE_FILES = COPYING
 
 define RAPTOR_IPC_BUILD_CMDS
-	$(MAKE) $(RAPTOR_IPC_MAKE_OPTS) -C $(@D) TARGET=$(SOC_FAMILY)
+	$(MAKE) $(RAPTOR_IPC_MAKE_OPTS) CROSS_COMPILE=$(TARGET_CROSS) \
+		TARGET=$(SOC_FAMILY) \
+		SOC_FAMILY=$(shell echo $(SOC_FAMILY) | tr a-z A-Z) \
+		SENSOR_MODEL=$(shell echo $(BR2_SENSOR_MODEL) | tr a-z A-Z) \
+		-C $(@D)
 endef
 
 define RAPTOR_IPC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/raptor $(TARGET_DIR)/usr/bin/raptor
+	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/bin $(@D)/raptor
 endef
 
 $(eval $(generic-package))
