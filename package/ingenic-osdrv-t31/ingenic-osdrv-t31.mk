c2b23c25efd12e46e5e2c69e4bbabb587ff2ebd3
gtxaspec <gtxent@gmail.com>
2024-03-02 04:13:09 -0800

    add prudynt package + thingino live 555

--- package/ingenic-osdrv-t31/ingenic-osdrv-t31.mk
+++ package/ingenic-osdrv-t31/ingenic-osdrv-t31.mk
@@ -9,6 +9,12 @@ INGENIC_OSDRV_T31_SITE =
 INGENIC_OSDRV_T31_LICENSE = MIT
 INGENIC_OSDRV_T31_LICENSE_FILES = LICENSE
 
+define INGENIC_OSDRV_T31_INSTALL_STAGING_CMDS
+	$(info Copying files to staging...)
+	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/lib
+	$(INSTALL) -m 644 $(@D)/files/lib/*.so $(STAGING_DIR)/usr/lib/
+endef
+
 define INGENIC_OSDRV_T31_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/sensor $(INGENIC_OSDRV_T31_PKGDIR)/files/sensor/$(SENSOR_MODEL).yaml
@@ -20,6 +26,10 @@ define INGENIC_OSDRV_T31_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/lib/ $(INGENIC_OSDRV_T31_PKGDIR)/files/lib/*.so
+endef
+
+define INGENIC_OSDRV_T31_POST_BUILD_HOOK
+	$(call INGENIC_OSDRV_T31_INSTALL_STAGING_CMDS)
 endef
 
 $(eval $(generic-package))
