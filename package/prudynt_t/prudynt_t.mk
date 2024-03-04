489f56f581cdbb417cab201a1c3f28290665f184
gtxaspec <gtxent@gmail.com>
2024-03-03 20:12:57 -0800

    rename prudynt; update configs

--- package/prudynt_t/prudynt_t.mk
+++ package/prudynt_t/prudynt_t.mk
@@ -1,7 +1,7 @@
-PRUDYNT_V3_SITE_METHOD = git
-PRUDYNT_V3_SITE = https://github.com/gtxaspec/prudynt-v3
-PRUDYNT_V3_VERSION = $(shell git ls-remote $(PRUDYNT_V3_SITE) HEAD | head -1 | cut -f1)
-PRUDYNT_V3_DEPENDENCIES = libconfig thingino-live555 ingenic-osdrv-t31 freetype thingino-fonts
+PRUDYNT_T_SITE_METHOD = git
+PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
+PRUDYNT_T_VERSION = $(shell git ls-remote $(PRUDYNT_T_SITE) HEAD | head -1 | cut -f1)
+PRUDYNT_T_DEPENDENCIES = libconfig thingino-live555 ingenic-osdrv-t31 freetype thingino-fonts
 
 # PRUDYNT_CFLAGS = $(TARGET_CLAGS)
 PRUDYNT_CFLAGS += -DNO_OPENSSL=1 -Og -g
@@ -16,14 +16,15 @@ PRUDYNT_LDFLAGS = $(TARGET_LDFLAGS)
 PRUDYNT_LDFLAGS += -L$(STAGING_DIR)/usr/lib
 PRUDYNT_LDFLAGS += -L$(TARGET_DIR)/usr/lib
 
-define PRUDYNT_V3_BUILD_CMDS
+define PRUDYNT_T_BUILD_CMDS
     $(MAKE) ARCH=$(TARGET_ARCH) CROSS_COMPILE=$(TARGET_CROSS) \
         CFLAGS="$(PRUDYNT_CFLAGS)" LDFLAGS="$(PRUDYNT_LDFLAGS)" -C $(@D) all
 endef
 
-define PRUDYNT_V3_INSTALL_TARGET_CMDS
+define PRUDYNT_T_INSTALL_TARGET_CMDS
     $(INSTALL) -m 0755 -D $(@D)/bin/prudynt $(TARGET_DIR)/usr/bin/prudynt
     $(INSTALL) -m 0644 -D $(@D)/prudynt.cfg.example $(TARGET_DIR)/etc/prudynt.cfg
+    $(INSTALL) -m 0755 -D $(PRUDYNT_T_PKGDIR)/files/S95prudynt $(TARGET_DIR)/etc/init.d/S95prudynt
 endef
 
 $(eval $(generic-package))
