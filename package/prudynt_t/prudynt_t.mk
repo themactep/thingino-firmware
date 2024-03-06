b44d3ba197701a42a2f74c7fabe19126efffba3a
gtxaspec <gtxent@gmail.com>
2024-03-05 23:50:38 -0800

    add deps to prudynt config.in

--- package/prudynt_t/prudynt_t.mk
+++ package/prudynt_t/prudynt_t.mk
@@ -1,13 +1,18 @@
 PRUDYNT_T_SITE_METHOD = git
 PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_VERSION = $(shell git ls-remote $(PRUDYNT_T_SITE) HEAD | head -1 | cut -f1)
-PRUDYNT_T_DEPENDENCIES = libconfig thingino-live555 ingenic-osdrv-t31 freetype thingino-fonts
+PRUDYNT_T_DEPENDENCIES = libconfig thingino-live555 freetype thingino-fonts
 
 # PRUDYNT_CFLAGS = $(TARGET_CLAGS)
 ifeq ($(SOC_FAMILY),t20)
 	PRUDYNT_CFLAGS += -DNO_OPENSSL=1 -O0 -DPLATFORM_T20
-else ifeq ($(SOC_FAMILY),t30)
+	PRUDYNT_T_DEPENDENCIES += ingenic-osdrv-t20
+else ifeq ($(SOC_FAMILY),t21)
+	PRUDYNT_CFLAGS += -DNO_OPENSSL=1 -O0 -DPLATFORM_T21
+	PRUDYNT_T_DEPENDENCIES += ingenic-osdrv-t21
+else ifeq ($(SOC_FAMILY),t31)
 	PRUDYNT_CFLAGS += -DNO_OPENSSL=1 -O2 -DPLATFORM_T31
+	PRUDYNT_T_DEPENDENCIES += ingenic-osdrv-t31
 endif
 PRUDYNT_CFLAGS += -I$(STAGING_DIR)/usr/include
 PRUDYNT_CFLAGS += -I$(STAGING_DIR)/usr/include/freetype2
