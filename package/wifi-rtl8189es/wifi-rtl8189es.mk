44878cdde2def87e0cc316e4d1160f3fa2122f5f
Paul Philippov <themactep@gmail.com>
2024-03-13 20:22:50 -0400

    fix rtl8189es package

--- package/wifi-rtl8189es/wifi-rtl8189es.mk
+++ package/wifi-rtl8189es/wifi-rtl8189es.mk
@@ -1,12 +1,13 @@
-WIFI_RTL8188ES_SITE_METHOD = git
-WIFI_RTL8188ES_SITE = https://github.com/jwrdegoede/rtl8189es_linux
-WIFI_RTL8188ES_VERSION = $(shell git ls-remote $(WIFI_RTL8188ES_SITE) master | head -1 | cut -f1)
+WIFI_RTL8189ES_SITE_METHOD = git
+WIFI_RTL8189ES_SITE = https://github.com/jwrdegoede/rtl8189es_linux.git
+WIFI_RTL8189ES_VERSION = $(shell git ls-remote $(WIFI_RTL8189ES_SITE) master | head -1 | cut -f1)
 
-WIFI_RTL8188ES_LICENSE = GPL-2.0
+WIFI_RTL8189ES_LICENSE = GPL-2.0
 
-WIFI_RTL8188ES_MODULE_MAKE_OPTS = CONFIG_RTL8189ES=m \
+WIFI_RTL8189ES_MODULE_MAKE_OPTS = \
+	KSRC=$(LINUX_DIR) \
 	KVER=$(LINUX_VERSION_PROBED) \
-	KSRC=$(LINUX_DIR)
+	CONFIG_RTL8189ES=m
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
