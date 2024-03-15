1f94c004ac33580e4a5c5691ec6552264c192aeb
Your Name <you@example.com>
2024-03-15 12:45:00 -0400

    change logcat package to use logcat-mini

--- package/logcat/logcat.mk
+++ package/logcat/logcat.mk
@@ -1,16 +1,15 @@
 LOGCAT_SITE_METHOD = git
-LOGCAT_SITE = https://github.com/gtxaspec/linux_logcat
+LOGCAT_SITE = https://github.com/wltechblog/logcat-mini
 LOGCAT_VERSION = $(shell git ls-remote $(LOGCAT_SITE) HEAD | head -1 | cut -f1)
 
-LOGCAT_LICENSE = GPL-2.0
-LOGCAT_LICENSE_FILES = COPYING
+LOGCAT_LICENSE = MIT
+LOGCAT_LICENSE_FILES = LICENSE
 
 LOGCAT_INSTALL_STAGING = YES
 
 LOGCAT_DEPENDENCIES = host-upx
 define LOGCAT_INSTALL_STAGING_CMDS
-        $(HOST_DIR)/bin/upx --best --lzma $(@D)/logcat/logcat
-        $(HOST_DIR)/bin/upx --best --lzma $(@D)/logwrapper/logwrapper
+        $(HOST_DIR)/bin/upx --best $(@D)/logcat
 endef
 
 $(eval $(cmake-package))
