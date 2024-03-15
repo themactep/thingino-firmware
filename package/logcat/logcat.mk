5ba68abb580ad70a657e7c89d8a1de5530a0ecbb
brokedown <wltechblog@wanderlounge.net>
2024-03-15 16:02:46 -0400

    Revert "change logcat package to use logcat-mini"
    This reverts commit f3fd47f6985a2580296b05a6eb12704f728759fd.

--- package/logcat/logcat.mk
+++ package/logcat/logcat.mk
@@ -1,15 +1,16 @@
 LOGCAT_SITE_METHOD = git
-LOGCAT_SITE = https://github.com/wltechblog/logcat-mini
+LOGCAT_SITE = https://github.com/gtxaspec/linux_logcat
 LOGCAT_VERSION = $(shell git ls-remote $(LOGCAT_SITE) HEAD | head -1 | cut -f1)
 
-LOGCAT_LICENSE = MIT
-LOGCAT_LICENSE_FILES = LICENSE
+LOGCAT_LICENSE = GPL-2.0
+LOGCAT_LICENSE_FILES = COPYING
 
 LOGCAT_INSTALL_STAGING = YES
 
 LOGCAT_DEPENDENCIES = host-upx
 define LOGCAT_INSTALL_STAGING_CMDS
-        $(HOST_DIR)/bin/upx --best $(@D)/logcat
+        $(HOST_DIR)/bin/upx --best --lzma $(@D)/logcat/logcat
+        $(HOST_DIR)/bin/upx --best --lzma $(@D)/logwrapper/logwrapper
 endef
 
 $(eval $(cmake-package))
