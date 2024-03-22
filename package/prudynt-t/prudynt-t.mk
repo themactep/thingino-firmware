76b13c5177039189e27e188f7599d476dd74aa7b
Paul Philippov <themactep@gmail.com>
2024-03-22 07:48:02 -0400

    deprecate sensor .yaml config

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -28,20 +28,12 @@ define PRUDYNT_T_BUILD_CMDS
         -C $(@D) all commit_tag=$(shell git show -s --format=%h)
 endef
 
-SENSOR_I2C_ADDRESS = $(shell awk '/address:/ {print $$2}' $(TARGET_DIR)/etc/sensor/$(SENSOR_MODEL).yaml)
-
-## TODO: this should be done better
-# SENSOR_FPS=$$(awk '/#define SENSOR_OUTPUT_MAX_FPS/ function nvl(x) {return x==""?"30":x} {print nvl($$3)}' $$(OUTPUT_DIR)/build/ingenic-sdk-*/sensor-src/$(SOC_FAMILY)/$(SENSOR_MODEL).c)
-# sed -i '/fps:/ s/24/$(SENSOR_FPS)/' $(TARGET_DIR)/etc/prudynt.cfg
-
 define PRUDYNT_T_INSTALL_TARGET_CMDS
     $(INSTALL) -m 0755 -D $(@D)/bin/prudynt $(TARGET_DIR)/usr/bin/prudynt
     $(INSTALL) -m 0644 -D $(@D)/prudynt.cfg.example $(TARGET_DIR)/etc/prudynt.cfg
     $(INSTALL) -m 0755 -D $(PRUDYNT_T_PKGDIR)files/S95prudynt $(TARGET_DIR)/etc/init.d/S95prudynt
     $(INSTALL) -m 0755 -D $(PRUDYNT_T_PKGDIR)files/S96record $(TARGET_DIR)/etc/init.d/S96record
     $(INSTALL) -m 0755 -D $(@D)/res/thingino_logo_1.bgra $(TARGET_DIR)/usr/share/thingino_logo_1.bgra
-    sed -i '/i2c_address:/ s/0x37/$(SENSOR_I2C_ADDRESS)/' $(TARGET_DIR)/etc/prudynt.cfg
-    sed -i '/model:/ s/"gc2053"/$(BR2_SENSOR_MODEL)/' $(TARGET_DIR)/etc/prudynt.cfg
 endef
 
 $(eval $(generic-package))
