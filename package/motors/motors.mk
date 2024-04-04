de0dc57a1bb0eab046ecf0db55faba29aae45f66
Paul Philippov <themactep@gmail.com>
2024-04-03 23:23:36 -0400

    fix compilation and installation of motors

--- package/motors/motors.mk
+++ package/motors/motors.mk
@@ -6,14 +6,14 @@ MOTORS_LICENSE = MIT
 MOTORS_LICENSE_FILES = LICENSE
 
 define MOTORS_BUILD_CMDS
-	$(TARGET_CC) -Os -s $(@D)/motor.c -o ingenic-motor
+	$(TARGET_CC) -Os -s $(@D)/motor.c -o $(@D)/motors
 endef
 
 define MOTORS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d/ $(MOTORS_PKGDIR)/files/S09motor
 
-	$(INSTALL) -m 0755 -D $(@D)/ingenic-motor/ingenic-motor $(TARGET_DIR)/usr/bin/motors
+	$(INSTALL) -m 0755 -D $(@D)/motors $(TARGET_DIR)/usr/bin/motors
 endef
 
 $(eval $(generic-package))
