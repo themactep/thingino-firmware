45e21d1fcc4127b7d60a4698035f7d5295225177
Paul Philippov <themactep@gmail.com>
2024-04-06 00:40:28 -0400

    eye candy

--- scripts/rootfs_script.sh
+++ scripts/rootfs_script.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 
-#NAME=Thingino
-#VERSION=1
-#ID=thingino
-#VERSION_ID=1
-#PRETTY_NAME="Thingino 1 (Ciao)"
-#ANSI_COLOR="1;34"
-#CPE_NAME="cpe:/o:thinginoproject:thingino:1"
-#HOME_URL="https://thingino.com/"
-#SUPPORT_URL="https://help.thingino.com/"
-#BUG_REPORT_URL="https://issues.thingino.com/"
-#PRIVACY_POLICY_URL="https://policy.thingino.com/"
-#BUILD_ID=
-#VARIANT="Ingenic T31 Edition"
-#VARIANT_ID="ingenic_t31"
-
-FILE=${TARGET_DIR}/usr/lib/os-release
+# get image id from the path to output
+IMAGE_ID=$(echo $BR2_CONFIG | awk -F '/' '{print $(NF-1)}')
 
 GIT_BRANCH=$(git branch | grep ^* | awk '{print $2}')
 GIT_HASH=$(git show -s --format=%H)
 GIT_TIME=$(git show -s --format=%ci)
+BUILD_ID="${GIT_BRANCH}+${GIT_HASH:0:7}, ${GIT_TIME}"
 
-echo "GITHUB_VERSION=\"${GIT_BRANCH}+${GIT_HASH}, ${GIT_TIME}\"" >>${FILE}
-date +TIME_STAMP=%s >>${FILE}
+FILE=${TARGET_DIR}/usr/lib/os-release
+# prefix exiting buildroot entires
+tmpfile=$(mktemp)
+sed 's/^/BUILDROOT_/' $FILE > $tmpfile
+# create our own release file
+{
+	echo "NAME=Thingino"
+	echo "ID=thingino"
+	echo "VERSION=\"1 (Ciao)\""
+	echo "VERSION_ID=1"
+	echo "VERSION_CODENAME=ciao"
+	echo "PRETTY_NAME=\"Thingino 1 (Ciao)\""
+	echo "ID_LIKE=buildroot"
+	echo "CPE_NAME=\"cpe:/o:thinginoproject:thingino:1\""
+	echo "LOGO=thingino-logo-icon"
+	echo "ANSI_COLOR=\"1;34\""
+	echo "HOME_URL=\"https://thingino.com/\""
+	echo "ARCHITECTURE=mips"
+	echo "IMAGE_ID=${IMAGE_ID}"
+	echo "BUILD_ID=\"${BUILD_ID}\""
+	date +TIME_STAMP=%s
+	cat $tmpfile
+} > $FILE
+rm $tmpfile
 
-if grep -q "^U_BOOT_ENV_TXT" ${BR2_CONFIG}; then
+if grep -q ^U_BOOT_ENV_TXT $BR2_CONFIG; then
 	uenv=$(sed -rn "s/^U_BOOT_ENV_TXT=\"\\\$\(\w+\)(.+)\"/\1/p" $BR2_CONFIG)
 	if [ -f "${BR2_EXTERNAL}${uenv}" ]; then
 		cp -v ${BR2_EXTERNAL}${uenv} ${TARGET_DIR}/etc/uenv.txt
 	fi
 fi
 
-if grep -q "USES_MUSL" ${BR2_CONFIG}; then
-#  LIST=${BR2_EXTERNAL}/scripts/excludes/${SOC_MODEL}.list
-#  test -e ${LIST} && xargs -a ${LIST} -I % rm -rf ${TARGET_DIR}/%
+if grep -q ^BR2_TOOLCHAIN_USES_MUSL $BR2_CONFIG; then
   ln -srf ${TARGET_DIR}/lib/libc.so ${TARGET_DIR}/lib/ld-uClibc.so.0
   ln -srf ${TARGET_DIR}/lib/libc.so ${TARGET_DIR}/usr/bin/ldd
 fi
