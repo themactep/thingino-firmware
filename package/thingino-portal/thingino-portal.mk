bd7b484d0c05080e89f031cf3a9a6c0d408309c2
gtxaspec <gtxent@gmail.com>
2024-05-01 08:30:24 -0700

    portal: update package makefile

--- package/thingino-portal/thingino-portal.mk
+++ package/thingino-portal/thingino-portal.mk
@@ -1,7 +1,7 @@
 define THINGINO_PORTAL_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0644 $(THINGINO_PORTAL_PKGDIR)/files/udhcpd.conf $(TARGET_DIR)/etc/udhcpd.conf
     $(INSTALL) -D -m 0644 $(THINGINO_PORTAL_PKGDIR)/files/wpa_ap.conf $(TARGET_DIR)/etc/wpa_ap.conf
-    $(INSTALL) -D -m 0755 $(THINGINO_PORTAL_PKGDIR)/files/S37portal $(TARGET_DIR)/etc/init.d/S37portal
+    $(INSTALL) -D -m 0755 $(THINGINO_PORTAL_PKGDIR)/files/S41portal $(TARGET_DIR)/etc/init.d/S41portal
 endef
 
 $(eval $(generic-package))
