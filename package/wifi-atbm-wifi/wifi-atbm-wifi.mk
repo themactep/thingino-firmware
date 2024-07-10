2964ceec1ea832d184d80da8b3b3d8cf2ff2b161
gtxaspec <gtxent@gmail.com>
2024-07-10 14:15:09 -0700

    package: add new wifi-atbm package and associated package fixes

---
+++ package/wifi-atbm-wifi/wifi-atbm-wifi.mk
@@ -0,0 +1,16 @@
+WIFI_ATBM_WIFI_SITE_METHOD = git
+WIFI_ATBM_WIFI_SITE = https://github.com/gtxaspec/atbm-wifi
+WIFI_ATBM_WIFI_SITE_BRANCH = master
+WIFI_ATBM_WIFI_VERSION = $(shell git ls-remote $(WIFI_ATBM_WIFI_SITE) $(WIFI_ATBM_WIFI_SITE_BRANCH) | head -1 | cut -f1)
+
+WIFI_ATBM_WIFI_LICENSE = GPL-2.0
+
+LINUX_CONFIG_LOCALVERSION = $(shell awk -F "=" '/^CONFIG_LOCALVERSION=/ {print $$2}' $(BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE))
+define WIFI_ATBM_WIFI_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/3.10.14$(LINUX_CONFIG_LOCALVERSION)
+	touch $(TARGET_DIR)/lib/modules/3.10.14$(LINUX_CONFIG_LOCALVERSION)/modules.builtin.modinfo
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/atbm_wifi
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/atbm_wifi $(WIFI_ATBM_WIFI_PKGDIR)/files/*.txt
+endef
+
+$(eval $(generic-package))
