3ccc33988a4e1fe3802893f90babbab03b1767cd
Paul Philippov <themactep@gmail.com>
2024-07-15 03:10:39 -0400

    rewrite binpadder script for more flexibility

--- scripts/binpadder.sh
+++ scripts/binpadder.sh
@@ -1,38 +1,55 @@
 #!/bin/bash
 #
 # Binary file padder.
-# Pads binary file with 0xFF to match full size of flashing chip.
+# Pads binary file with 0xFF to a given size.
+# Size can be in given as bytes, or with a suffix:
+# 16000, 256k, 8m
 #
 # Example:
-#   ./binpadder.sh u-boot-t10.bin 8
-#
-# Running this command will produce a new binary file
-# u-boot-t10-8MB-padded.bin
-# in the same directory as the original binary file.
+# ./binpadder.sh u-boot-t10.bin 8M
 #
 # Paul Philippov <paul@themactep.com>
 #
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <binary file> <flash chip size in MB>"
+  echo "Usage: $0 <binary file> <final size>"
   exit 1
 fi
 
-case "$2" in
-   8) flashsizemb="8MB"; flashsize=8388608 ;;
-  16) flashsizemb="16MB"; flashsize=16777216 ;;
-   *) echo "Unknown flash size. Use 8 or 16." && exit 2
+
+unit=${2:0-1}
+echo "SIZE: $2"
+echo "UNIT: $unit"
+
+
+case "$unit" in
+   b|B|0|1|2|3|4|5|6|7|8|9|0)
+    break
+    final_size=$2 ;;
+   k|K)
+    final_size=$((${2::-1} * 1024))
+    break
+    ;;
+   m|M)
+    final_size=$((${2::-1} * 1024 * 1024))
+    break
+    ;;
+   *)
+    echo "$unit - Unknown unit"
+    exit 2
+    ;;
 esac
 
-infile="$1"
-[ ! -f "$infile" ] && echo "Cannot find input binary file." && exit 3
-infilesize=$(wc -c $infile | awk '{print $1}')
+input_file="$1"
+[ ! -f "$input_file" ] && echo "Cannot find input binary file." && exit 3
 
-[ "$infilesize" -gt "$flashsize" ] && echo "Binary file is larger than targeted flash size!" && exit 4
+input_file_size=$(wc -c $input_file | awk '{print $1}')
 
-blankfile="${infile%.*}-${flashsizemb}-blank.bin"
-dd if=/dev/zero bs="${flashsize}" skip=0 count=1 | tr '\000' '\377' > "${blankfile}"
-dd if="${infile}" bs=1 skip=0 count=${infilesize} of="${blankfile}" conv=notrunc status=none
-mv "${blankfile}" "${infile%.*}-${flashsizemb}-padded.bin"
+[ "$input_file_size" -gt "$final_size" ] && echo "Binary file is larger than targeted flash size!" && exit 4
+
+temp_file=$(mktemp)
+dd if=/dev/zero bs=$final_size skip=0 count=1 | tr '\000' '\377' > $temp_file
+dd if="$input_file" bs=1 skip=0 count=$input_file_size of=$temp_file conv=notrunc status=none
+mv $temp_file "${input_file%.*}-$2-padded.bin"
 
 exit 0
