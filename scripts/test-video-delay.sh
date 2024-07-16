1f110a5e58136fa6e3b23eb64be248acc381bb91
Paul Philippov <themactep@gmail.com>
2024-07-15 23:23:51 -0400

    video test: drop support of openipc, refactor usage syntax.

--- scripts/test-video-delay.sh
+++ scripts/test-video-delay.sh
@@ -4,7 +4,7 @@
 # Use it together with https://thingino.com/timer
 #
 # 2023, Paul Philippov <paul@themactep.com>
-#
+# 2024-07-15: Rewrite for easier use.
 
 if ! command -v gst-launch-1.0 > /dev/null; then
 	echo "This script requires GStreamer."
@@ -12,57 +12,26 @@ if ! command -v gst-launch-1.0 > /dev/null; then
 	exit 1
 fi
 
-protocol="$1"
-codec="$2"
-
 show_help_and_exit() {
-	echo "Usage: $0 [rtmp|rtsp] [h264|h265] [rtsp://thingino:thingino@192.168.1.10:554/ch0]"
+	echo "Usage: $0 <rtsp://thingino:thingino@192.168.1.10:554/ch0> [h264|h265]"
 	exit 1
 }
 
-case "$protocol" in
-	"rtmp")
-		echo "This only works with OpenIPC, not Thingino!"
-		case "$codec" in
-			"h265")
-				gst-launch-1.0 -vvv udpsrc port=5600 \
-					caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H265' \
-					! rtpjitterbuffer ! rtph265depay ! avdec_h265 ! glimagesink sync=false
-				;;
-			"h264")
-				gst-launch-1.0 -vvv udpsrc port=5600 \
-					caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' \
-					! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! glimagesink sync=false
-				;;
-			*)
-				echo "Unknown codec"
-				exit 1
-				;;
-		esac
-		echo "Add these lines to /etc/majestic.yaml:"
-		echo "outgoing:"
-		echo "  - udp://$(hostname -I | awk '{print $1}'):5600"
+url="$1"
+[ -z "$url" ] && show_help_and_exit
+
+codec="$2"
+case "$codec" in
+	"h265")
+		gst-launch-1.0 -vvv rtspsrc location=$url ! queue ! \
+			rtph265depay ! h265parse ! avdec_h265 ! fpsdisplaysink sync=false name=v
 		;;
-	"rtsp")
-		url="$3"
-		[ -z "$url" ] && show_help_and_exit
-		case "$codec" in
-			"h264")
-				gst-launch-1.0 -vvv rtspsrc location=$url ! queue ! \
-					rtph264depay ! h264parse ! avdec_h264 ! fpsdisplaysink sync=false name=v
-				;;
-			"h265")
-				gst-launch-1.0 -vvv rtspsrc location=$url ! queue ! \
-					rtph265depay ! h265parse ! avdec_h265 ! fpsdisplaysink sync=false name=v
-				;;
-			*)
-				echo "Unknown codec"
-				show_help_and_exit
-				;;
-		esac
+	"h264"|"")
+		gst-launch-1.0 -vvv rtspsrc location=$url ! queue ! \
+			rtph264depay ! h264parse ! avdec_h264 ! fpsdisplaysink sync=false name=v
 		;;
 	*)
-		echo "Unknown protocol"
+		echo "Unknown codec"
 		show_help_and_exit
 		;;
 esac
