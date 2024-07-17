1c16fd036d2c3861e863aa728f7abfe849f78197
Paul Philippov <themactep@gmail.com>
2024-07-17 19:54:12 -0400

    uboot: switch to the main repo for t23 builds

--- external.mk
+++ external.mk
@@ -580,13 +580,8 @@ $(info KERNEL_HASH=$(shell git ls-remote $(KERNEL_SITE) $(KERNEL_BRANCH) | head 
 THINGINO_KERNEL = $(KERNEL_SITE)/archive/$(KERNEL_HASH).tar.gz
 $(info THINGINO_KERNEL=$(THINGINO_KERNEL))
 
-ifeq ($(BR2_SOC_INGENIC_T23),y)
-THINGINO_UBOOT_REPO = https://github.com/themactep/thingino-u-boot
-THINGINO_UBOOT_REPO_BRANCH = resolved1
-else
 THINGINO_UBOOT_REPO = https://github.com/gtxaspec/u-boot-ingenic
 THINGINO_UBOOT_REPO_BRANCH = master
-endif
 THINGINO_UBOOT_REPO_VERSION = $(shell git ls-remote $(THINGINO_UBOOT_REPO) $(THINGINO_UBOOT_REPO_BRANCH) | head -1 | cut -f1)
 $(info THINGINO_UBOOT_REPO=$(THINGINO_UBOOT_REPO))
 $(info THINGINO_UBOOT_REPO_BRANCH=$(THINGINO_UBOOT_REPO_BRANCH))
