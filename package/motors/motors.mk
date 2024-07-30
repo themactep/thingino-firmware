fe5a6b3734a03997238e77a1a3f6b213a18be039
roleoroleo <roleo@iperbole.bologna.it>
2024-07-30 12:35:53 +0200

    Add support for motor withe inverted_gpio_dir

--- package/motors/motors.mk
+++ package/motors/motors.mk
@@ -11,12 +11,25 @@ define MOTORS_BUILD_CMDS
 	$(TARGET_CC) -Os -s $(@D)/motor-daemon.c -o $(@D)/motors-daemon
 endef
 
+ifeq ($(BR2_MOTORS_INVERT_GPIO_DIR),y)
 define MOTORS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d/ $(MOTORS_PKGDIR)/files/S09motor
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/modprobe.d
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/modprobe.d/ $(MOTORS_PKGDIR)/files/motor.conf
+
+	$(INSTALL) -m 0755 -D $(@D)/motors $(TARGET_DIR)/usr/bin/motors
+	$(INSTALL) -m 0755 -D $(@D)/motors-daemon $(TARGET_DIR)/usr/bin/motors-daemon
+endef
+else
+define MOTORS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d/ $(MOTORS_PKGDIR)/files/S09motor
 
 	$(INSTALL) -m 0755 -D $(@D)/motors $(TARGET_DIR)/usr/bin/motors
 	$(INSTALL) -m 0755 -D $(@D)/motors-daemon $(TARGET_DIR)/usr/bin/motors-daemon
 endef
+endif
 
 $(eval $(generic-package))
