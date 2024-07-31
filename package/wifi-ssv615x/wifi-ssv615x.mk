36625d46d5c9fc717c5d10b50eef726b9ee1c6aa
Disappear9 <disappear9@outlook.com>
2024-07-31 14:51:17 +0800

    Add wifi-ssv615x

---
+++ package/wifi-ssv615x/wifi-ssv615x.mk
@@ -0,0 +1,13 @@
+WIFI_SSV615X_SITE_METHOD = git
+WIFI_SSV615X_SITE = https://github.com/openipc/ssv6x5x
+WIFI_SSV615X_SITE_BRANCH = ssv615x
+WIFI_SSV615X_VERSION = $(shell git ls-remote $(WIFI_SSV615X_SITE) $(WIFI_SSV615X_SITE_BRANCH) | head -1 | cut -f1)
+
+WIFI_SSV615X_LICENSE = GPL-2.0
+WIFI_SSV615X_LICENSE_FILES = COPYING
+
+WIFI_SSV615X_MODULE_MAKE_OPTS = \
+	KSRC=$(LINUX_DIR)
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
