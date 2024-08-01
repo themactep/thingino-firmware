e355db607dd78bbfdaf4610f36c98da16d724790
gtxaspec <gtxent@gmail.com>
2024-08-01 05:20:08 -0700

    wifi package: change config path to /usr/share/wifi

--- package/wifi-hi3881/wifi-hi3881.mk
+++ package/wifi-hi3881/wifi-hi3881.mk
@@ -36,11 +36,11 @@ LINUX_CONFIG_LOCALVERSION = $(shell awk -F "=" '/^CONFIG_LOCALVERSION=/ {print $
 define WIFI_HI3881_INSTALL_CONFIGS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/modules/$(FULL_KERNEL_VERSION)$(call qstrip,$(LINUX_CONFIG_LOCALVERSION))
 	touch $(TARGET_DIR)/lib/modules/$(FULL_KERNEL_VERSION)$(call qstrip,$(LINUX_CONFIG_LOCALVERSION))/modules.builtin.modinfo
-        $(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/hi3881-wifi
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/hi3881-wifi $(@D)/firmware/wifi_cfg/fcc/wifi_cfg
-        $(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware
-        $(INSTALL) -m 644 $(@D)/firmware/hi3881_fw.bin $(TARGET_DIR)/lib/firmware/hi3881_fw.bin
-	sed -i 's|ADDR_FILE_FILES=1,0xe4800,/vendor/firmware/hisilicon/hi3881_fw.bin;|ADDR_FILE_FILES=1,0xe4800,/lib/firmware/hi3881_fw.bin;|' $(TARGET_DIR)/usr/share/hi3881-wifi/wifi_cfg
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/wifi
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/wifi $(@D)/firmware/wifi_cfg/fcc/wifi_cfg
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware
+	$(INSTALL) -m 644 $(@D)/firmware/hi3881_fw.bin $(TARGET_DIR)/lib/firmware/hi3881_fw.bin
+	sed -i 's|ADDR_FILE_FILES=1,0xe4800,/vendor/firmware/hisilicon/hi3881_fw.bin;|ADDR_FILE_FILES=1,0xe4800,/lib/firmware/hi3881_fw.bin;|' $(TARGET_DIR)/usr/share/wifi/wifi_cfg
 endef
 
 WIFI_HI3881_POST_INSTALL_TARGET_HOOKS += WIFI_HI3881_INSTALL_CONFIGS
