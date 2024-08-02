f1709d42dc3b1926af33574a1624a4c401786f7a
Paul Philippov <themactep@gmail.com>
2024-08-02 00:55:12 -0400

    webui: add mjpeg-frame source and compilation

---
+++ package/thingino-webui/files/mjpeg_frame.c
@@ -0,0 +1,42 @@
+/*
+ * Write a single MJPEG frame, open the file and getting its size atomically.
+ * This assumes that any process updating the file will do so via renaming a
+ * temporary file (rather than writing to the file directly).
+ *
+ * Returns success if the entire file was sent, and failure otherwise.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define CRLF "\r\n"
+
+static char buf[65536];
+
+int
+main(int ac, char *av[])
+{
+    if (ac <= 1) {
+        fprintf(stderr, "Usage: %s filename.jpeg [frame_boundary]\n", av[0]);
+        return EXIT_FAILURE;
+    }
+    char *jpeg = av[1];
+    char *boundary = ac > 2 ? av[2] : "frame";
+    int jpegfd = open(jpeg, O_RDONLY);
+    struct stat st;
+    fstat(jpegfd, &st);
+    printf("--%s" CRLF, boundary);
+    printf("Content-Type: image/jpeg" CRLF);
+    printf("Content-Length: %jd" CRLF CRLF, (intmax_t) st.st_size);
+    fflush(stdout);
+    int bread;
+    while ((bread = read(jpegfd, buf, sizeof buf)) > 0) {
+        int bwritten = write(1, buf, bread);
+        if (bwritten != bread)
+            return EXIT_FAILURE;
+    }
+    return bread == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
--- package/thingino-webui/thingino-webui.mk
+++ package/thingino-webui/thingino-webui.mk
@@ -3,12 +3,20 @@ THINGINO_WEBUI_SITE = $(THINGINO_WEBUI_PKGDIR)/files
 THINGINO_WEBUI_LICENSE = MIT
 THINGINO_WEBUI_LICENSE_FILES = LICENSE
 
+define THINGINO_WEBUI_BUILD_CMDS
+	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -std=c99 -pedantic \
+		-o $(@D)/mjpeg_frame $(@D)/mjpeg_frame.c
+endef
+#-O2 -Wl,-z -Wl,max-page-size=4096 \
+
 define THINGINO_WEBUI_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc $(THINGINO_WEBUI_PKGDIR)/files/etc/httpd.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(THINGINO_WEBUI_PKGDIR)/files/etc/init.d/S50httpd
+
+	$(INSTALL) -m 0755 -D $(@D)/mjpeg_frame $(TARGET_DIR)/usr/bin/mjpeg_frame
 
 	if grep -q "^BR2_THINGINO_DEV_PACKAGES=y" $(BR2_CONFIG); then \
 		$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(THINGINO_WEBUI_PKGDIR)/files/etc/init.d/S44devmounts; \
