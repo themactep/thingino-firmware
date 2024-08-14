8976ae827c22e4b1c2c610c477bf32ab1d9c7ef5
Nils Schimmelmann <nschimme@gmail.com>
2024-08-13 20:29:11 -0500

    onvif-simple-server: add onvif_notify_server and track motion alarm

--- package/onvif-simple-server/onvif-simple-server.mk
+++ package/onvif-simple-server/onvif-simple-server.mk
@@ -24,6 +24,7 @@ endef
 
 define ONVIF_SIMPLE_SERVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(@D)/onvif_notify_server
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(@D)/onvif_simple_server
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(@D)/wsd_simple_server
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/ptz_presets
@@ -31,13 +32,18 @@ define ONVIF_SIMPLE_SERVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/etc $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/onvif.conf
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/etc $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/ptz_presets.conf
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/init.d
-	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/S96onvif
+	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/S96onvif_discovery
+	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(ONVIF_SIMPLE_SERVER_PKGDIR)/files/S97onvif_notify
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/device_service_files
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/device_service_files $(@D)/device_service_files/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/events_service_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/events_service_files $(@D)/events_service_files/*
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/generic_files
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/generic_files $(@D)/generic_files/*
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/media_service_files
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/media_service_files $(@D)/media_service_files/*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/notify_files
+	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/notify_files $(@D)/notify_files/*
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/ptz_service_files
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/ptz_service_files $(@D)/ptz_service_files/*
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/wsd_files
