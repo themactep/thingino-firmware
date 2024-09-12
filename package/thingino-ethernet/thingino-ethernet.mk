e8d11fc0286c44eca94b6db3920cf3c863ba3aa5
Paul Philippov <themactep@gmail.com>
2024-09-11 21:10:54 -0400

    ethernet: revert moving of eth0 to the package level, that breaks usb ethernet support.

--- package/thingino-ethernet/thingino-ethernet.mk
+++ package/thingino-ethernet/thingino-ethernet.mk
@@ -1,6 +1,6 @@
-define THINGINO_ETHERNET_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/network/interfaces.d
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/network/interfaces.d/ $(THINGINO_ETHERNET_PKGDIR)/files/eth0
-endef
+#define THINGINO_ETHERNET_INSTALL_TARGET_CMDS
+#	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/network/interfaces.d
+#	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/network/interfaces.d/ $(THINGINO_ETHERNET_PKGDIR)/files/eth0
+#endef
 
 $(eval $(generic-package))
