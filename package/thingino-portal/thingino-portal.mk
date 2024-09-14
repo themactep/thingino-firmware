2fadcb88276f0e66e0cb2341a3d02eed377f3c01
Paul Philippov <themactep@gmail.com>
2024-09-14 09:51:49 -0400

    web portal: compress assets

--- package/thingino-portal/thingino-portal.mk
+++ package/thingino-portal/thingino-portal.mk
@@ -15,13 +15,15 @@ define THINGINO_PORTAL_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/var/www-portal/x
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/var/www-portal/x/ $(THINGINO_PORTAL_PKGDIR)/files/index.cgi
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/var/www-portal/x/ $(THINGINO_PORTAL_PKGDIR)/files/portal.cgi
+        find $(TARGET_DIR)/var/www-portal/x/ -type f -name *.cgi -exec chmod 755 {} \;
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/var/www/a
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/var/www/a $(THINGINO_PORTAL_PKGDIR)/files/bootstrap.bundle.min.js
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/var/www/a $(THINGINO_PORTAL_PKGDIR)/files/bootstrap.min.css
+        find $(TARGET_DIR)/var/www/a/ -type f \( -name "*.css" -o -name "*.ico" -o -name "*.js" -o -name "*.svg" \) -exec gzip {} \;
 
-	ln -sr $(TARGET_DIR)/var/www/a/bootstrap.min.css       $(TARGET_DIR)/var/www-portal/
-	ln -sr $(TARGET_DIR)/var/www/a/bootstrap.bundle.min.js $(TARGET_DIR)/var/www-portal/
+	ln -sr $(TARGET_DIR)/var/www/a/bootstrap.min.css.gz       $(TARGET_DIR)/var/www-portal/
+	ln -sr $(TARGET_DIR)/var/www/a/bootstrap.bundle.min.js.gz $(TARGET_DIR)/var/www-portal/
 endef
 
 $(eval $(generic-package))
