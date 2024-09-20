549e550d4410767a272d1227b2b0adfcfbb83204
Paul Philippov <themactep@gmail.com>
2024-09-20 04:56:42 -0400

    thingino-devscripts: collect scripts for development to run on camera side.

---
+++ package/thingino-devscripts/thingino-devscripts.mk
@@ -0,0 +1,8 @@
+define THINGINO_DEVSCRIPTS_INSTALL_TARGET_CMDS
+        $(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/sbin
+        $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/daylightsample
+        $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/gpioscan
+        $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/ticklemotor
+endef
+
+$(eval $(generic-package))
