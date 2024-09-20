e7d2d59f3f241bb38e0a0bb5af85cac82f1a4de4
gtxaspec <gtxent@gmail.com>
2024-09-20 09:03:59 -0700

    thingino-devscripts: install overlay-backup

--- package/thingino-devscripts/thingino-devscripts.mk
+++ package/thingino-devscripts/thingino-devscripts.mk
@@ -3,6 +3,7 @@ define THINGINO_DEVSCRIPTS_INSTALL_TARGET_CMDS
         $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/daylightsample
         $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/gpioscan
         $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/ticklemotor
+        $(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DEVSCRIPTS_PKGDIR)/files/overlay-backup
 endef
 
 $(eval $(generic-package))
