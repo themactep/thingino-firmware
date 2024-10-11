dcf6a38b3f585081059494ee133e986880dbaba9
themactep <themactep@users.noreply.github.com>
2024-10-11 06:02:09 +0000

    Update thingino-live555 version from 2024.10.10 to 2024.10.11

--- package/thingino-live555/thingino-live555.mk
+++ package/thingino-live555/thingino-live555.mk
@@ -1,4 +1,4 @@
-THINGINO_LIVE555_VERSION = 2024.10.10
+THINGINO_LIVE555_VERSION = 2024.10.11
 THINGINO_LIVE555_SOURCE = live.$(THINGINO_LIVE555_VERSION).tar.gz
 THINGINO_LIVE555_SITE = http://www.live555.com/liveMedia/public
 # There is a COPYING file with the GPL-3.0 license text, but none of
