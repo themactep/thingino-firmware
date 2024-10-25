de2c32b147f6fd0ff74fd1ff12d11525f57fe988
jim smith <47976944+jimsmt@users.noreply.github.com>
2024-10-25 14:39:58 +0800

    Update wifi-aic8800.mk

--- package/wifi-aic8800/wifi-aic8800.mk
+++ package/wifi-aic8800/wifi-aic8800.mk
@@ -5,7 +5,7 @@ WIFI_AIC8800_VERSION = $(shell git ls-remote $(WIFI_AIC8800_SITE) $(WIFI_AIC8800
 
 WIFI_AIC8800_LICENSE = GPL-2.0
 
-define WIFI_ATBM6062S_LINUX_CONFIG_FIXUPS
+define WIFI_AIC8800_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_WLAN)
 	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
 	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS_EXT)
@@ -19,6 +19,18 @@ define WIFI_ATBM6062S_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_MAC80211_RC_DEFAULT_MINSTREL)
 	$(call KCONFIG_SET_OPT,CONFIG_MAC80211_RC_DEFAULT,"minstrel_ht")
 endef
+
+define WIFI_AIC8800_INSTALL_FIRMWARE
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/lib/firmware/aic8800D80
+	$(INSTALL) -m 644 $(@D)/fw/aic8800D80/fw_patch_table_8800d80_u02.bin $(TARGET_DIR)/lib/firmware/aic8800D80/fw_patch_table_8800d80_u02.bin
+	$(INSTALL) -m 644 $(@D)/fw/aic8800D80/fw_adid_8800d80_u02.bin $(TARGET_DIR)/lib/firmware/aic8800D80/fw_adid_8800d80_u02.bin
+	$(INSTALL) -m 644 $(@D)/fw/aic8800D80/fw_patch_8800d80_u02.bin $(TARGET_DIR)/lib/firmware/aic8800D80/fw_patch_8800d80_u02.bin
+	$(INSTALL) -m 644 $(@D)/fw/aic8800D80/fw_patch_table_8800d80_u02.bin $(TARGET_DIR)/lib/firmware/aic8800D80/fw_patch_table_8800d80_u02.bin
+	$(INSTALL) -m 644 $(@D)/fw/aic8800D80/fw_patch_8800d80_u02_ext0.bin $(TARGET_DIR)/lib/firmware/aic8800D80/fw_patch_8800d80_u02_ext0.bin
+	$(INSTALL) -m 644 $(@D)/fw/aic8800D80/aic_userconfig_8800d80.txt $(TARGET_DIR)/lib/firmware/aic8800D80/aic_userconfig_8800d80.txt
+endef
+
+WIFI_AIC8800_POST_INSTALL_TARGET_HOOKS += WIFI_AIC8800_INSTALL_FIRMWARE
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
