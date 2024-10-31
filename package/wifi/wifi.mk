85ca4a27dfda44dcceae9864ab3a31669d0d1bb9
gtxaspec <gtxent@gmail.com>
2024-10-31 04:13:40 +0000

    wlan: move to wifi package

--- package/wifi/wifi.mk
+++ package/wifi/wifi.mk
@@ -3,6 +3,14 @@ define WIFI_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d/ $(WIFI_PKGDIR)/files/S36wireless
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d/ $(WIFI_PKGDIR)/files/S38wpa_supplicant
 
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin/
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlan
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlanrssi
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlantemp
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlaninfo
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlansetup
+
+
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/network/interfaces.d
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/network/interfaces.d/ $(WIFI_PKGDIR)/files/wlan0
 
