88e6e3b13ca2899afd53fb454923dad618ef8176
gtxaspec <gtxent@gmail.com>
2024-11-07 08:41:50 -0800

    package: add thingino-ap

---
+++ package/thingino-ap/thingino-ap.mk
@@ -0,0 +1,13 @@
+define THINGINO_AP_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/dnsd-ap.conf
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/udhcpd-ap.conf
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/wpa-ap.conf
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d/ $(THINGINO_AP_PKGDIR)/files/S42wifiap
+endef
+
+THINGINO_AP_POST_INSTALL_TARGET_HOOKS += MODIFY_INSTALL_CONFIGS
+
+$(eval $(generic-package))
