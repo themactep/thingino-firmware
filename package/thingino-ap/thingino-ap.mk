d5ba8b4f0ad6efc70474674c47f4ece30850eadd
Paul Philippov <themactep@gmail.com>
2024-11-09 01:29:43 -0500

    thingino-ap: add dns resolver config

--- package/thingino-ap/thingino-ap.mk
+++ package/thingino-ap/thingino-ap.mk
@@ -2,6 +2,7 @@ define THINGINO_AP_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/dnsd-ap.conf
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/udhcpd-ap.conf
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/resolv-ap.conf
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(THINGINO_AP_PKGDIR)/files/wpa-ap.conf
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
