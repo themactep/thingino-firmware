aa61e9dca4ca716617e9486bde5653b852b71d52
Paul Philippov <themactep@gmail.com>
2024-11-11 04:27:53 -0500

    wifi: create symlinks in the target directory, otherwise install creates copies of files.

--- package/wifi/wifi.mk
+++ package/wifi/wifi.mk
@@ -5,12 +5,11 @@ define WIFI_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin/
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlan
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlancli
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlanrssi
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlantemp
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlaninfo
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(WIFI_PKGDIR)/files/wlansetup
-
+	ln -sr $(TARGET_DIR)/usr/sbin/wlan $(TARGET_DIR)/usr/sbin/wlancli
+	ln -sr $(TARGET_DIR)/usr/sbin/wlan $(TARGET_DIR)/usr/sbin/wlaninfo
+	ln -sr $(TARGET_DIR)/usr/sbin/wlan $(TARGET_DIR)/usr/sbin/wlanrssi
+	ln -sr $(TARGET_DIR)/usr/sbin/wlan $(TARGET_DIR)/usr/sbin/wlansetup
+	ln -sr $(TARGET_DIR)/usr/sbin/wlan $(TARGET_DIR)/usr/sbin/wlantemp
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/network/interfaces.d
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/network/interfaces.d/ $(WIFI_PKGDIR)/files/wlan0
