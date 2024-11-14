46aa08547494ea8a070c47d016043e63e14290bf
gtxaspec <gtxent@gmail.com>
2024-11-14 08:41:02 -0800

    user-menu/dep_check: simplify

--- scripts/dep_check.sh
+++ scripts/dep_check.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 check_glibc_version() {
-	local min_version="2.31"
+	min_version="2.31"
 	# Check if running on Alpine with musl
 	if ldd --version 2>&1 | grep -q musl; then
 		echo "Alpine Linux detected with musl libc."
 		return 0
 	fi
 
-	local current_version=$(ldd --version | head -n1 | grep -oE '[0-9]+\.[0-9]+' | head -1)
+	current_version=$(ldd --version | head -n1 | grep -oE '[0-9]+\.[0-9]+' | head -1)
 	if [ "$(printf '%s\n' "$min_version" "$current_version" | sort -V | head -n1)" = "$min_version" ]; then
 		echo "glibc version is $current_version, which is >= $min_version."
 	else
-		echo -e "glibc version is $current_version, which is less than $min_version.\nGLIBC Upgrade required, which usually means you need to upgrade the Operating System."
+		echo "glibc version is $current_version, which is less than $min_version."
+		echo "GLIBC Upgrade required, which usually means you need to upgrade the Operating System."
 		exit 1
 	fi
 }
@@ -26,7 +27,8 @@ install_packages() {
 	fi
 }
 
-if command -v sudo &> /dev/null; then
+# Check if sudo is available
+if command -v sudo >/dev/null 2>&1; then
 	install_cmd="sudo"
 else
 	install_cmd=""
@@ -34,222 +36,109 @@ fi
 
 if [ -f /etc/os-release ]; then
 	. /etc/os-release
-	OS=$NAME
+	OS="$NAME"
 
 	# Check ID_LIKE for Debian-based identification first
-	if [[ "$ID_LIKE" =~ debian ]]; then
-		echo "Detected as Debian-based via ID_LIKE"
-		pkg_manager="dpkg"
-		pkg_check_command="dpkg-query -W -f='${Status}'"
-		pkg_install_cmd="apt-get install -y"
-		pkg_update_cmd="apt-get update"
-		declare -A packages=(
-			[build-essential]='build-essential'
-			[bc]='bc'
-			[bison]='bison'
-			[cpio]='cpio'
-			[cmake]='cmake'
-			[curl]='curl'
-			[file]='file'
-			[flex]='flex'
-			[gawk]='gawk'
-			[git]='git'
-			[libncurses-dev]='libncurses-dev'
-			[make]='make'
-			[rsync]='rsync'
-			[unzip]='unzip'
-			[u-boot-tools]='u-boot-tools'
-			[wget]='wget'
-			[whiptail]='whiptail'
-			[dialog]='dialog'
-		)
-	else
-		# Check specific IDs if ID_LIKE didn't catch it
-		case "$ID" in
-			ubuntu|debian|linuxmint|zorin)
-				echo "Detected as Debian-based via ID"
-				pkg_manager="dpkg"
-				pkg_check_command="dpkg-query -W -f='${Status}'"
-				pkg_install_cmd="apt-get install -y"
-				pkg_update_cmd="apt-get update"
-				declare -A packages=(
-					[build-essential]='build-essential'
-					[bc]='bc'
-					[bison]='bison'
-					[cpio]='cpio'
-					[cmake]='cmake'
-					[curl]='curl'
-					[file]='file'
-					[flex]='flex'
-					[gawk]='gawk'
-					[git]='git'
-					[libncurses-dev]='libncurses-dev'
-					[make]='make'
-					[rsync]='rsync'
-					[unzip]='unzip'
-					[u-boot-tools]='u-boot-tools'
-					[wget]='wget'
-					[whiptail]='whiptail'
-					[dialog]='dialog'
-				)
-				;;
-			rhel|centos|fedora)
-				echo "RedHat-based"
-				pkg_manager="rpm"
-				pkg_check_command="rpm -q"
-				pkg_install_cmd="dnf install -y"
-				declare -A packages=(
-					[gcc]='gcc'
-					[make]='make'
-					[bc]='bc'
-					[bison]='bison'
-					[cpio]='cpio'
-					[cmake]='cmake'
-					[curl]='curl'
-					[file]='file'
-					[flex]='flex'
-					[gawk]='gawk'
-					[git]='git'
-					[ncurses-devel]='ncurses-devel'
-					[rsync]='rsync'
-					[unzip]='unzip'
-					[wget]='wget'
-					[newt]='newt'
-					[dialog]='dialog'
-				)
-				;;
-			arch)
-				echo "Arch-based"
-				pkg_manager="pacman"
-				pkg_check_command="pacman -Q"
-				pkg_install_cmd="pacman -S --noconfirm"
-				declare -A packages=(
-					[base-devel]='base-devel'
-					[bc]='bc'
-					[bison]='bison'
-					[cpio]='cpio'
-					[cmake]='cmake'
-					[curl]='curl'
-					[file]='file'
-					[flex]='flex'
-					[gawk]='gawk'
-					[git]='git'
-					[ncurses]='ncurses'
-					[make]='make'
-					[rsync]='rsync'
-					[unzip]='unzip'
-					[wget]='wget'
-					[libnewt]='libnewt'
-					[dialog]='dialog'
-				)
-				;;
-			alpine)
-				echo "Alpine Linux"
-				pkg_manager="apk"
-				pkg_check_command="apk info -e"
-				pkg_install_cmd="apk add"
-				declare -A packages=(
-					[build-base]='build-base'
-					[bc]='bc'
-					[bison]='bison'
-					[cpio]='cpio'
-					[cmake]='cmake'
-					[curl]='curl'
-					[file]='file'
-					[flex]='flex'
-					[gawk]='gawk'
-					[git]='git'
-					[ncurses-dev]='ncurses-dev'
-					[make]='make'
-					[rsync]='rsync'
-					[unzip]='unzip'
-					[wget]='wget'
-					[newt]='newt'
-					[dialog]='dialog'
-					[perl]='perl'
-					[findutils]='findutils'
-					[grep]='grep'
-				)
-				;;
-			opensuse*)
-				echo "OpenSUSE Tumbleweed"
-				pkg_manager="zypper"
-				pkg_check_command="zypper search -i"
-				pkg_install_cmd="zypper install -y"
-				declare -A packages=(
-					[gcc]='gcc'
-					[make]='make'
-					[bc]='bc'
-					[bison]='bison'
-					[cpio]='cpio'
-					[cmake]='cmake'
-					[curl]='curl'
-					[file]='file'
-					[flex]='flex'
-					[gawk]='gawk'
-					[git]='git'
-					[ncurses-devel]='ncurses-devel'
-					[rsync]='rsync'
-					[unzip]='unzip'
-					[wget]='wget'
-					[newt]='newt'
-					[dialog]='dialog'
-					[perl]='perl'
-					[findutils]='findutils'
-					[grep]='grep'
-				)
-				;;
-			*)
-				echo "Unsupported OS: $ID"
-				exit 1
-				;;
-		esac
-	fi
+	case "$ID_LIKE" in
+		*debian*)
+			echo "Detected as Debian-based via ID_LIKE"
+			pkg_manager="dpkg"
+			pkg_check_command="dpkg-query -W -f='${Status}'"
+			pkg_install_cmd="apt-get install -y"
+			pkg_update_cmd="apt-get update"
+			packages="build-essential bc bison cpio cmake curl file flex gawk git libncurses-dev make rsync unzip u-boot-tools wget whiptail dialog"
+			;;
+		*)
+			case "$ID" in
+				ubuntu|debian|linuxmint|zorin)
+					echo "Detected as Debian-based via ID"
+					pkg_manager="dpkg"
+					pkg_check_command="dpkg-query -W -f='${Status}'"
+					pkg_install_cmd="apt-get install -y"
+					pkg_update_cmd="apt-get update"
+					packages="build-essential bc bison cpio cmake curl file flex gawk git libncurses-dev make rsync unzip u-boot-tools wget whiptail dialog"
+					;;
+				rhel|centos|fedora)
+					echo "RedHat-based"
+					pkg_manager="rpm"
+					pkg_check_command="rpm -q"
+					pkg_install_cmd="dnf install -y"
+					packages="gcc make bc bison cpio cmake curl file flex gawk git ncurses-devel rsync unzip wget newt dialog"
+					;;
+				arch)
+					echo "Arch-based"
+					pkg_manager="pacman"
+					pkg_check_command="pacman -Q"
+					pkg_install_cmd="pacman -S --noconfirm"
+					packages="base-devel bc bison cpio cmake curl file flex gawk git ncurses make rsync unzip wget libnewt dialog"
+					;;
+				alpine)
+					echo "Alpine Linux"
+					pkg_manager="apk"
+					pkg_check_command="apk info -e"
+					pkg_install_cmd="apk add"
+					packages="bash build-base bc bison cpio cmake curl file flex gawk git ncurses-dev make rsync unzip wget newt dialog perl findutils grep"
+					;;
+				opensuse*)
+					echo "OpenSUSE Tumbleweed"
+					pkg_manager="zypper"
+					pkg_check_command="zypper search -i"
+					pkg_install_cmd="zypper install -y"
+					packages="gcc make bc bison cpio cmake curl file flex gawk git ncurses-devel rsync unzip wget newt dialog perl findutils grep"
+					;;
+				*)
+					echo "Unsupported OS: $ID"
+					exit 1
+					;;
+			esac
+			;;
+	esac
 else
 	echo "Could not determine the operating system."
 	exit 1
 fi
 
+# Check glibc version
 check_glibc_version
 
 # Check installed packages based on the package manager
-packages_to_install=()
+packages_to_install=""
 
-for key in "${!packages[@]}"; do
+for pkg in $packages; do
 	case "$pkg_manager" in
 		dpkg)
-			if ! dpkg-query -W -f='${Status}' "${packages[$key]}" 2>/dev/null | grep -q "install ok installed"; then
-				packages_to_install+=("${packages[$key]}")
+			if ! dpkg-query -W -f='${Status}' "$pkg" 2>/dev/null | grep -q "install ok installed"; then
+				packages_to_install="$packages_to_install $pkg"
 			else
-				echo "Package ${packages[$key]} is installed."
+				echo "Package $pkg is installed."
 			fi
 			;;
 		rpm)
-			if ! rpm -q "${packages[$key]}" &>/dev/null; then
-				packages_to_install+=("${packages[$key]}")
+			if ! rpm -q "$pkg" >/dev/null 2>&1; then
+				packages_to_install="$packages_to_install $pkg"
 			else
-				echo "Package ${packages[$key]} is installed."
+				echo "Package $pkg is installed."
 			fi
 			;;
 		pacman)
-			if ! pacman -Q "${packages[$key]}" &>/dev/null; then
-				packages_to_install+=("${packages[$key]}")
+			if ! pacman -Q "$pkg" >/dev/null 2>&1; then
+				packages_to_install="$packages_to_install $pkg"
 			else
-				echo "Package ${packages[$key]} is installed."
+				echo "Package $pkg is installed."
 			fi
 			;;
 		apk)
-			if ! apk info -e "${packages[$key]}" &>/dev/null; then
-				packages_to_install+=("${packages[$key]}")
+			if ! apk info -e "$pkg" >/dev/null 2>&1; then
+				packages_to_install="$packages_to_install $pkg"
 			else
-				echo "Package ${packages[$key]} is installed."
+				echo "Package $pkg is installed."
 			fi
 			;;
 		zypper)
-			if ! zypper search -i "${packages[$key]}" &>/dev/null; then
-				packages_to_install+=("${packages[$key]}")
+			if ! zypper search -i "$pkg" >/dev/null 2>&1; then
+				packages_to_install="$packages_to_install $pkg"
 			else
-				echo "Package ${packages[$key]} is installed."
+				echo "Package $pkg is installed."
 			fi
 			;;
 		*)
@@ -259,15 +148,17 @@ for key in "${!packages[@]}"; do
 	esac
 done
 
-if [ ${#packages_to_install[@]} -ne 0 ]; then
+# Install missing packages if any
+if [ -n "$packages_to_install" ]; then
 	echo "The following packages are missing and need to be installed:"
-	for pkg in "${packages_to_install[@]}"; do
+	for pkg in $packages_to_install; do
 		echo "- $pkg"
 	done
 
-	read -p "Do you wish to proceed with the installation? (yes/no): " user_input
+	echo "Do you wish to proceed with the installation? (yes/no): "
+	read user_input
 	if [ "$user_input" = "yes" ] || [ "$user_input" = "y" ]; then
-		if [ "$EUID" -ne 0 ] && [ -z "$install_cmd" ]; then
+		if [ "$(id -u)" -ne 0 ] && [ -z "$install_cmd" ]; then
 			echo "This script needs superuser privileges to install packages."
 			echo "Please run it with sudo or as root."
 			exit 1
@@ -276,7 +167,7 @@ if [ ${#packages_to_install[@]} -ne 0 ]; then
 			echo "Updating package list..."
 			$install_cmd $pkg_update_cmd
 		fi
-		install_packages "${packages_to_install[@]}"
+		install_packages $packages_to_install
 	else
 		echo "Installation aborted by the user."
 		exit 0
