976e356a77f8578b66e67a2819690622686e84f9
gtxaspec <gtxent@gmail.com>
2024-12-20 21:59:59 -0800

    ifplugd: remove from busybox config; add as a fixup in thingino-ethernet

--- package/thingino-ethernet/thingino-ethernet.mk
+++ package/thingino-ethernet/thingino-ethernet.mk
@@ -3,4 +3,14 @@
 #	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/network/interfaces.d/ $(THINGINO_ETHERNET_PKGDIR)/files/eth0
 #endef
 
+ifeq ($(BR2_ETHERNET),y)
+define THINGINO_ETHERNET_BUSYBOX_CONFIG_FIXUPS_ETH
+        $(call KCONFIG_ENABLE_OPT,CONFIG_IFPLUGD)
+endef
+endif
+
+define THINGINO_ETHERNET_BUSYBOX_CONFIG_FIXUPS
+        $(call KCONFIG_ENABLE_OPT,CONFIG_IFPLUGD)
+endef
+
 $(eval $(generic-package))
