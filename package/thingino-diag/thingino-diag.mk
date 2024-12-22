5551abe43ced24252281bb3de1970c19a6b1abd9
gtxaspec <gtxent@gmail.com>
2024-12-22 03:54:20 -0800

    thingino-diag: install soc from here

--- package/thingino-diag/thingino-diag.mk
+++ package/thingino-diag/thingino-diag.mk
@@ -1,6 +1,7 @@
 define THINGINO_DIAG_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/thingino-diag
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/soc
 endef
 
 $(eval $(generic-package))
