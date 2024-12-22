1dac56c853ef38a8e7ee7210f3954220214ccc05
gtxaspec <gtxent@gmail.com>
2024-12-22 03:44:21 -0800

    thingino-gpio: install gpio from this package

--- package/thingino-gpio/thingino-gpio.mk
+++ package/thingino-gpio/thingino-gpio.mk
@@ -4,6 +4,7 @@ THINGINO_GPIO_SITE = $(BR2_EXTERNAL)/package/thingino-gpio
 define THINGINO_GPIO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/files/gpio.conf $(TARGET_DIR)/etc/gpio.conf
 	$(INSTALL) -D -m 0755 $(@D)/files/S05gpio $(TARGET_DIR)/etc/init.d/S05gpio
+	$(INSTALL) -D -m 0755 $(@D)/files/gpio $(TARGET_DIR)/usr/sbin/gpio
 endef
 
 define INSTALL_GPIO_CONF
