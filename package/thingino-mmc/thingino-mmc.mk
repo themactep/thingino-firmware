5f64147fd8267a7fd828cb335569af4a14b8e39e
gtxaspec <gtxent@gmail.com>
2024-12-22 03:47:30 -0800

    thingino-mmc: install mmc from here

--- package/thingino-mmc/thingino-mmc.mk
+++ package/thingino-mmc/thingino-mmc.mk
@@ -1,6 +1,8 @@
 define THINGINO_MMC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(THINGINO_MMC_PKGDIR)/files/S09mmc
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_MMC_PKGDIR)/files/mmc
 endef
 
 $(eval $(generic-package))
