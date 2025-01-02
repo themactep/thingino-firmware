bb4418d275bbbc03bbff81c9805df55343f5fc17
Paul Philppov <paul@themactep.com>
2025-01-02 15:23:14 -0500

    prudynt: unpin to use the latest commit.

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -1,30 +1,30 @@
 PRUDYNT_T_SITE_METHOD = git
 ifeq ($(BR2_PACKAGE_PRUDYNT_T_NG),y)
-    PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
-    PRUDYNT_T_SITE_BRANCH = master
-    PRUDYNT_T_VERSION = 6eab9c0ef6fac8eb80f10ce489bca18295d84729
-    # $(shell git ls-remote $(PRUDYNT_T_SITE) $(PRUDYNT_T_SITE_BRANCH) | head -1 | cut -f1)
+	PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
+	PRUDYNT_T_SITE_BRANCH = master
+	#PRUDYNT_T_VERSION = 6eab9c0ef6fac8eb80f10ce489bca18295d84729
+	PRUDYNT_T_VERSION = $(shell git ls-remote $(PRUDYNT_T_SITE) $(PRUDYNT_T_SITE_BRANCH) | head -1 | cut -f1)
 else
-    PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
-    PRUDYNT_T_SITE_BRANCH = prudynt-t-old
-    PRUDYNT_T_VERSION = 5daadef8f84596fd39343a5a794ebfd419c225fb
-    # $(shell git ls-remote $(PRUDYNT_T_SITE) $(PRUDYNT_T_SITE_BRANCH) | head -1 | cut -f1)
+	PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
+	PRUDYNT_T_SITE_BRANCH = prudynt-t-old
+	#PRUDYNT_T_VERSION = 5daadef8f84596fd39343a5a794ebfd419c225fb
+	PRUDYNT_T_VERSION = $(shell git ls-remote $(PRUDYNT_T_SITE) $(PRUDYNT_T_SITE_BRANCH) | head -1 | cut -f1)
 endif
 
 
 PRUDYNT_T_DEPENDENCIES = libconfig thingino-live555 thingino-fonts ingenic-lib faac thingino-opus
 ifeq ($(BR2_PACKAGE_PRUDYNT_T_NG),y)
-PRUDYNT_T_DEPENDENCIES += libwebsockets libschrift
+	PRUDYNT_T_DEPENDENCIES += libwebsockets libschrift
 else
-PRUDYNT_T_DEPENDENCIES += thingino-freetype
+	PRUDYNT_T_DEPENDENCIES += thingino-freetype
 endif
 ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
-PRUDYNT_T_DEPENDENCIES += ingenic-musl
+	PRUDYNT_T_DEPENDENCIES += ingenic-musl
 endif
 
 PRUDYNT_CFLAGS += -DPLATFORM_$(shell echo $(SOC_FAMILY) | tr a-z A-Z)
 ifeq ($(KERNEL_VERSION_4),y)
-PRUDYNT_CFLAGS += -DKERNEL_VERSION_4
+	PRUDYNT_CFLAGS += -DKERNEL_VERSION_4
 endif
 
 PRUDYNT_CFLAGS += \
