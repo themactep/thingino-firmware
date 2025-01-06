46c70a0d659e564d0520accb411ab1d5f2afc9dd
Sergi Esteve <ecce.pecto@gmail.com>
2025-01-06 20:39:20 +0100

    wifi-wq9001: fix calibration file intallation

--- package/wifi-wq9001/wifi-wq9001.mk
+++ package/wifi-wq9001/wifi-wq9001.mk
@@ -25,10 +25,12 @@ define WIFI_WQ9001_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_SET_OPT,CONFIG_MAC80211_RC_DEFAULT,"minstrel_ht")
 endef
 
-define WIFI_WQ9001_INSTALL_TARGET_HOOKS
+define WIFI_WQ9001_INSTALL_CALIBRATION_FILE
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/share/wifi
-	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/wifi/WQ9001_WiFi_Mapfile.dat $(@D)/cal/WQ9001_WiFi_Mapfile.dat
+	$(INSTALL) -m 644 -t $(TARGET_DIR)/usr/share/wifi/ $(@D)/cal/WQ9001_WiFi_Mapfile.dat
 endef
+
+WIFI_WQ9001_POST_INSTALL_TARGET_HOOKS += WIFI_WQ9001_INSTALL_CALIBRATION_FILE
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
