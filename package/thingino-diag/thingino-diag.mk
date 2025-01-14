2ed344afdf0960eed9de564e4ef4d3213bcdfb72
gtxaspec <gtxent@gmail.com>
2025-01-13 19:47:30 -0800

    thingino-diag: install to rootfs

--- package/thingino-diag/thingino-diag.mk
+++ package/thingino-diag/thingino-diag.mk
@@ -2,6 +2,7 @@ define THINGINO_DIAG_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/sbin
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/thingino-diag
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/soc
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/sensor
 endef
 
 $(eval $(generic-package))
