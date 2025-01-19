40721d908614e7cdec6047dbdfcfbd4134093ae1
gtxaspec <gtxent@gmail.com>
2025-01-18 23:07:32 -0800

    thingino-diag: move various utilities out of overlay

--- package/thingino-diag/thingino-diag.mk
+++ package/thingino-diag/thingino-diag.mk
@@ -3,6 +3,10 @@ define THINGINO_DIAG_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/thingino-diag
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/soc
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/sensor
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/overlay-backup
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/ispmem-calc
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/firstboot
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/usb-role
 endef
 
 $(eval $(generic-package))
