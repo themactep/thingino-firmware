cbebd9c120bd93de618fe0f5f1c2627f240f3003
gtxaspec <gtxent@gmail.com>
2025-02-22 20:23:10 -0800

    go2rtc: add init script

--- package/go2rtc/go2rtc.mk
+++ package/go2rtc/go2rtc.mk
@@ -24,6 +24,8 @@ define GO2RTC_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/go2rtc $(TARGET_DIR)/usr/bin/go2rtc
 	$(HOST_DIR)/bin/upx --best --lzma $(TARGET_DIR)/usr/bin/go2rtc
 	$(INSTALL) -m 644 -t $(TARGET_DIR)/etc/ $(GO2RTC_PKGDIR)/files/go2rtc.yaml
+	$(INSTALL) -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(GO2RTC_PKGDIR)/files/S97go2rtc
 endef
 
 $(eval $(golang-package))
