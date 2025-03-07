c981907a40a696b9c9c66f713ce0cff21dda5f6c
gtxaspec <gtxent@gmail.com>
2025-03-06 19:36:07 -0800

    ingenic-lib: update paths for A1

--- package/ingenic-lib/ingenic-lib.mk
+++ package/ingenic-lib/ingenic-lib.mk
@@ -8,7 +8,7 @@ INGENIC_LIB_INSTALL_STAGING = YES
 INGENIC_LIB_LICENSE = GPL-2.0
 INGENIC_LIB_LICENSE_FILES = COPYING
 
-ifeq ($(BR2_SOC_FAMILY_INGENIC_T40)$(BR2_SOC_FAMILY_INGENIC_T41),y)
+ifeq ($(BR2_SOC_FAMILY_INGENIC_T40)$(BR2_SOC_FAMILY_INGENIC_T41)$(BR2_SOC_FAMILY_INGENIC_A1),y)
 	LIBALOG_FILE = $(@D)/$(SOC_FAMILY_CAPS)/lib/$(SDK_VERSION)/$(SDK_LIBC_NAME)/libalog.so
 else
 	# Install libalog.so from T31 1.1.6 for every XBurst1 SoC
@@ -18,7 +18,7 @@ endif
 
 define INGENIC_LIB_INSTALL_STAGING_CMDS
 	$(INSTALL) -m 755 -d $(STAGING_DIR)/usr/lib
-        if [ "$(BR2_SOC_FAMILY_INGENIC_T40)" = "y" ] || [ "$(BR2_SOC_FAMILY_INGENIC_T41)" = "y" ]; then \
+        if [ "$(BR2_SOC_FAMILY_INGENIC_T40)" = "y" ] || [ "$(BR2_SOC_FAMILY_INGENIC_T41)" = "y" || [ "$(BR2_SOC_FAMILY_INGENIC_A1)" = "y" ]; then \
 		$(INSTALL) -m 644 -t $(STAGING_DIR)/usr/lib/ $(@D)/$(SOC_FAMILY_CAPS)/lib/$(SDK_VERSION)/$(SDK_LIBC_NAME)/*.so; \
 	else \
 		$(INSTALL) -m 644 -t $(STAGING_DIR)/usr/lib/ $(@D)/$(SOC_FAMILY_CAPS)/lib/$(SDK_VERSION)/$(SDK_LIBC_NAME)/$(SDK_LIBC_VERSION)/*.so; \
