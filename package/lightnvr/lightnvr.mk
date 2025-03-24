52a404f1dfe14d397610b8ded68f53c74d449d7f
gtxaspec <gtxent@gmail.com>
2025-03-23 21:49:01 -0700

    lightnvr: add init script

--- package/lightnvr/lightnvr.mk
+++ package/lightnvr/lightnvr.mk
@@ -3,20 +3,20 @@ LIGHTNVR_SITE = https://github.com/opensensor/lightNVR
 LIGHTNVR_SITE_BRANCH = main
 LIGHTNVR_VERSION = $(shell git ls-remote $(LIGHTNVR_SITE) $(LIGHTNVR_SITE_BRANCH) | head -1 | cut -f1)
 
-LIGHTNVR_LICENSE = GPL-2.0
+LIGHTNVR_LICENSE = MIT
 LIGHTNVR_LICENSE_FILES = COPYING
 
 LIGHTNVR_INSTALL_STAGING = YES
 
 LIGHTNVR_DEPENDENCIES = thingino-ffmpeg thingino-libcurl sqlite
 
-define LIGHTNVR_INSTALL_CONFIGS
+define LIGHTNVR_INSTALL_TARGET_CMDS
 	$(INSTALL) -d $(TARGET_DIR)/var/nvr
 	cp -r $(@D)/web $(TARGET_DIR)/var/nvr/
-	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/lightnvr
 	$(INSTALL) -m 644 $(@D)/config/lightnvr.ini $(TARGET_DIR)/etc/lightnvr/lightnvr.ini
+	$(INSTALL) -D -m 0755 $(@D)/lightnvr $(TARGET_DIR)/usr/bin/lightnvr
+	$(INSTALL) -m 0755 -D $(LIGHTNVR_PKGDIR)/files/S95lightnvr $(TARGET_DIR)/etc/init.d/S95lightnvr
 endef
-LIGHTNVR_POST_INSTALL_TARGET_HOOKS += LIGHTNVR_INSTALL_CONFIGS
-
 
 $(eval $(cmake-package))
