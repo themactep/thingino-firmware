809ca1c2c7cf7738de14d3bc42f9a3618bbbc0bb
gtxaspec <gtxent@gmail.com>
2025-03-23 22:16:19 -0700

    thingino-diag: add sinfo

--- package/thingino-diag/thingino-diag.mk
+++ package/thingino-diag/thingino-diag.mk
@@ -3,6 +3,7 @@ define THINGINO_DIAG_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/thingino-diag
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/soc
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/sensor
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/sensor-info
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/overlay-backup
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/ispmem-calc
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/sbin $(THINGINO_DIAG_PKGDIR)/files/firstboot
