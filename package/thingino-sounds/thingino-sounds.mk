55a3254e3efd0be1246d48fd16b7b9789f687207
gtxaspec <gtxent@gmail.com>
2025-04-27 00:53:21 -0700

    thingino-sounds: update device type variable name

--- package/thingino-sounds/thingino-sounds.mk
+++ package/thingino-sounds/thingino-sounds.mk
@@ -8,7 +8,7 @@ define THINGINO_SOUNDS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/sounds \
 		$(THINGINO_SOUNDS_PKGDIR)/files/th-chime_*.pcm
 
-	if [ "$(BR2_THINGINO_DEVICE_TYPE_DOORBELL)" = "y" ]; then \
+	if [ "$(BR2_THINGINO_DEV_DOORBELL)" = "y" ]; then \
 		$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/sounds \
 			$(THINGINO_SOUNDS_PKGDIR)/files/th-doorbell_*.pcm; \
 	fi
