e4a6e24883b75c19408a6f9656d2b7af92e099f7
Paul Philippov <paul@themactep.com>
2025-09-01 13:50:26 -0400

    thingino-fonts: simplify fonts installation

--- package/thingino-fonts/thingino-fonts.mk
+++ package/thingino-fonts/thingino-fonts.mk
@@ -3,14 +3,10 @@ THINGINO_FONTS_SITE = $(BR2_EXTERNAL)/package/thingino-fonts
 THINGINO_FONTS_LICENSE = MIT
 THINGINO_FONTS_LICENSE_FILES = LICENSE
 
-define THINGINO_FONTS_EXTRACT_CMDS
-	cp $(THINGINO_FONTS_PKGDIR)/files/* $(@D)/
-endef
-
 define THINGINO_FONTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/fonts/
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/fonts/ \
-		$(@D)/*.ttf
+		$(THINGINO_FONTS_PKGDIR)/files/*.ttf
 endef
 
 $(eval $(generic-package))
