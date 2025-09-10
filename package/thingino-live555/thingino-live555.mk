8664110e817b6fe91c008357aa8fb8e496e89bb0
Paul Philippov <paul@themactep.com>
2025-09-10 03:55:03 -0400

    live555: switch to our own fork

--- package/thingino-live555/thingino-live555.mk
+++ package/thingino-live555/thingino-live555.mk
@@ -1,7 +1,7 @@
 THINGINO_LIVE555_SITE_METHOD = git
-THINGINO_LIVE555_SITE = https://github.com/themactep/live555
+THINGINO_LIVE555_SITE = https://github.com/themactep/thingino-live555
 THINGINO_LIVE555_SITE_BRANCH = master
-THINGINO_LIVE555_VERSION = a9360c4627bcc47d05fee891520045c88aa7982a
+THINGINO_LIVE555_VERSION = 019a63d27e5620238c548321d763b79001e4c9cf
 # $(shell git ls-remote $(THINGINO_LIVE555_SITE) $(THINGINO_LIVE555_SITE_BRANCH) | head -1 | cut -f1)
 
 # There is a COPYING file with the GPL-3.0 license text, but none of
