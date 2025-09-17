62667b0f11dab0ec4033fbb41cb1c6ceb5a7a17d
Paul Philippov <paul@themactep.com>
2025-09-16 21:31:06 -0400

    clean up disabled dynamic commits tracking

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -3,7 +3,6 @@ PRUDYNT_T_SITE_METHOD = git
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = master
 PRUDYNT_T_VERSION = e78d72ead24e4a980a8a627ec28090841b14013f
-# $(shell git ls-remote $(PRUDYNT_T_SITE) $(PRUDYNT_T_SITE_BRANCH) | head -1 | cut -f1)
 
 PRUDYNT_T_GIT_SUBMODULES = YES
 
