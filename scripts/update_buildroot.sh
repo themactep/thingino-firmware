def314686e84a3ab036d4725a83a5a93091f19c7
Paul Philippov <paul@themactep.com>
2025-09-20 21:48:33 -0400

    refactor script to update buildroot submodule

--- scripts/update_buildroot.sh
+++ scripts/update_buildroot.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 # Update buildroot submodule to the latest version
-git pull
-git submodule update --remote --merge
+
+if [ -n "$1" ]; then
+    cd buildroot
+    git checkout $1 || { echo "Failed to checkout $1"; exit 1; }
+    cd ..
+    git add buildroot
+    git commit -m "Update buildroot to version $1"
+else
+    git submodule update --remote buildroot
+    git add buildroot
+    git commit -m "Update buildroot to latest version"
+fi
