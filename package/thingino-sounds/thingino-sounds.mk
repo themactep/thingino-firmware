c1b9df2f472b1f487d66159957cdba7be2ca6598
Paul Philippov <paul@themactep.com>
2025-09-20 23:36:03 -0400

    sounds: make startup sound optional

--- package/thingino-sounds/thingino-sounds.mk
+++ package/thingino-sounds/thingino-sounds.mk
@@ -4,11 +4,15 @@ THINGINO_SOUNDS_LICENSE = CC0
 THINGINO_SOUNDS_LICENSE_FILES = LICENSE
 
 define THINGINO_SOUNDS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(THINGINO_SOUNDS_PKGDIR)/files/thingino.pcm \
-		$(TARGET_DIR)/usr/share/sounds/thingino.pcm
+	$(INSTALL) -d $(TARGET_DIR)/usr/share/sounds
 
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/sounds \
 		$(THINGINO_SOUNDS_PKGDIR)/files/th-chime_*.pcm
+
+	if [ "$(BR2_PACKAGE_THINGINO_SOUNDS_STARTUP)" = "y" ]; then \
+		$(INSTALL) -m 0644 $(THINGINO_SOUNDS_PKGDIR)/files/thingino.pcm \
+			$(TARGET_DIR)/usr/share/sounds/thingino.pcm; \
+	fi
 
 	if [ "$(BR2_THINGINO_DEV_DOORBELL)" = "y" ]; then \
 		$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/sounds \
