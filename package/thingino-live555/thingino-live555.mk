7e445ae53f6306126e0bddd5f8144eece4cad4cb
Paul Philippov <paul@themactep.com>
2025-09-26 19:15:40 -0400

    live555: bump version

--- package/thingino-live555/thingino-live555.mk
+++ package/thingino-live555/thingino-live555.mk
@@ -1,8 +1,7 @@
 THINGINO_LIVE555_SITE_METHOD = git
 THINGINO_LIVE555_SITE = https://github.com/themactep/live555
 THINGINO_LIVE555_SITE_BRANCH = master
-THINGINO_LIVE555_VERSION = a9360c4627bcc47d05fee891520045c88aa7982a
-# $(shell git ls-remote $(THINGINO_LIVE555_SITE) $(THINGINO_LIVE555_SITE_BRANCH) | head -1 | cut -f1)
+THINGINO_LIVE555_VERSION = 25574c8efadbc2c1b54bd5a6649303de6b2d6830
 
 # There is a COPYING file with the GPL-3.0 license text, but none of
 # the source files appear to be released under GPL-3.0, and the
