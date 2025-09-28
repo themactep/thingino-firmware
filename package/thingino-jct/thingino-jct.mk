ef6598e8d4b0bdc234f852d8bf65008c68ad827f
Paul Philippov <themactep@gmail.com>
2025-09-28 06:45:53 -0400

    prudynt: replace cjson with jct library

--- package/thingino-jct/thingino-jct.mk
+++ package/thingino-jct/thingino-jct.mk
@@ -7,12 +7,31 @@ THINGINO_JCT_LICENSE = MIT
 THINGINO_JCT_LICENSE_FILES = LICENSE
 
 define THINGINO_JCT_BUILD_CMDS
-	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D)
+	$(MAKE) CROSS_COMPILE=$(TARGET_CROSS) LDFLAGS="$(TARGET_LDFLAGS)" -C $(@D) lib jct
+endef
+
+define THINGINO_JCT_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/libjct.a \
+		$(STAGING_DIR)/usr/lib/libjct.a
+	$(INSTALL) -D -m 0755 $(@D)/libjct.so \
+		$(STAGING_DIR)/usr/lib/libjct.so.1.0.0
+	$(INSTALL) -D -m 0644 $(@D)/src/json_config.h \
+		$(STAGING_DIR)/usr/include/json_config.h
+	ln -sf libjct.so.1.0.0 $(STAGING_DIR)/usr/lib/libjct.so.1
+	ln -sf libjct.so.1.0.0 $(STAGING_DIR)/usr/lib/libjct.so
 endef
 
 define THINGINO_JCT_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/jct \
 		$(TARGET_DIR)/usr/bin/jct
+	$(INSTALL) -D -m 0755 $(@D)/libjct.so \
+		$(TARGET_DIR)/usr/lib/libjct.so.1.0.0
+	$(INSTALL) -D -m 0644 $(@D)/libjct.a \
+		$(TARGET_DIR)/usr/lib/libjct.a
+	$(INSTALL) -D -m 0644 $(@D)/src/json_config.h \
+		$(TARGET_DIR)/usr/include/json_config.h
+	ln -sf libjct.so.1.0.0 $(TARGET_DIR)/usr/lib/libjct.so.1
+	ln -sf libjct.so.1.0.0 $(TARGET_DIR)/usr/lib/libjct.so
 endef
 
 $(eval $(generic-package))
