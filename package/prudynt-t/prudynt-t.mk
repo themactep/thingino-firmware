e66eaf75edc8fe41fe5e5544ef9a2b55230328c2
Paul Philippov <themactep@gmail.com>
2025-09-29 02:14:37 -0400

    prudynt: bump

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 #PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 05a6969c26021fcd3fd0edb9a6197fa75dc13436
+PRUDYNT_T_VERSION = eabbf885e789797d290ca36c2142919f94ef6ab6
 
 PRUDYNT_T_GIT_SUBMODULES = YES
 
