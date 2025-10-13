4415180e642472915aefacb3a2ebdd48e1337192
Matt Davis <matteius@gmail.com>
2025-10-13 10:24:07 -0400

    Fix the go2rtc compilation

--- package/go2rtc/go2rtc.mk
+++ package/go2rtc/go2rtc.mk
@@ -2,12 +2,12 @@ GO2RTC_SITE_METHOD = git
 ifeq ($(BR2_PACKAGE_GO2RTC_MINI),y)
 	GO2RTC_SITE = https://github.com/wltechblog/go2rtc-smaller
 	GO2RTC_SITE_BRANCH = master
-	GO2RTC_VERSION = 49b7e2c1b74b9fc4e529d2a1626ab67489eecc7c
+	GO2RTC_VERSION = df95ce39d08f4eae0544f7dc340f8d8ee27a5752
 	# $(shell git ls-remote $(GO2RTC_SITE) $(GO2RTC_SITE_BRANCH) | head -1 | cut -f1)
 else
 	GO2RTC_SITE = https://github.com/AlexxIT/go2rtc
 	GO2RTC_SITE_BRANCH = master
-	GO2RTC_VERSION = d99bf122eae07eccec159e6496f0aea4295597db
+	GO2RTC_VERSION = df95ce39d08f4eae0544f7dc340f8d8ee27a5752
 	# $(shell git ls-remote $(GO2RTC_SITE) $(GO2RTC_SITE_BRANCH) | head -1 | cut -f1)
 endif
 
@@ -16,9 +16,15 @@ GO2RTC_LICENSE_FILES = LICENSE
 
 GO2RTC_INSTALL_TARGET = YES
 
-GO2RTC_DEPENDENCIES = host-upx
-GO2RTC_GO_ENV = GOARCH=mipsle
+GO2RTC_DEPENDENCIES = host-upx toolchain
 GO2RTC_LDFLAGS = -s -w" -gcflags=all="-l -B
+GO2RTC_GO_ENV = \
+    GOARCH=mipsle \
+    CGO_ENABLED=1 \
+    CC=$(TARGET_CC) \
+    CXX=$(TARGET_CXX) \
+    CGO_CFLAGS="$(TARGET_CFLAGS)" \
+    CGO_LDFLAGS="$(TARGET_LDFLAGS)"
 
 define GO2RTC_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(GO2RTC_PKGDIR)/files/go2rtc.yaml \
@@ -31,6 +37,22 @@ define GO2RTC_INSTALL_TARGET_CMDS
 		$(TARGET_DIR)/usr/bin/go2rtc
 
 	$(HOST_DIR)/bin/upx --best --lzma $(TARGET_DIR)/usr/bin/go2rtc
+endef
+
+define GO2RTC_BUILD_CMDS
+    cd $(@D); \
+    GOARCH=mipsle \
+    CGO_ENABLED=0 \
+    $(HOST_DIR)/bin/go build -v \
+        -ldflags "-s -w" \
+        -gcflags=all="-l -B" \
+        -tags "noalsa nov4l2" \
+        -modcacherw \
+        -trimpath \
+        -p $(PARALLEL_JOBS) \
+        -buildvcs=false \
+        -o $(@D)/bin/go2rtc \
+        github.com/AlexxIT/go2rtc/.
 endef
 
 $(eval $(golang-package))
