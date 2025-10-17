994624b8e8d5ecb89e6e1ffe81fb98d7e40b6b3c
Paul Philippov <paul@themactep.com>
2025-10-16 20:29:26 -0400

    sounds: set correct permissions on the directory

--- package/thingino-sounds/thingino-sounds.mk
+++ package/thingino-sounds/thingino-sounds.mk
@@ -4,7 +4,7 @@ THINGINO_SOUNDS_LICENSE = CC0
 THINGINO_SOUNDS_LICENSE_FILES = LICENSE
 
 define THINGINO_SOUNDS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d $(TARGET_DIR)/usr/share/sounds
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/sounds
 
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/sounds \
 		$(THINGINO_SOUNDS_PKGDIR)/files/th-chime_*.pcm
