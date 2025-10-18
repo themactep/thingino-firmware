db5712af000587cdf83e87802ddbf262aff78de5
Paul Philippov <paul@themactep.com>
2025-10-18 09:51:15 -0400

    add nino editor

---
+++ package/nino/nino.mk
@@ -0,0 +1,39 @@
+################################################################################
+# nino
+################################################################################
+
+NINO_VERSION = c3e34047d025bbd913886844f8749e6ff925c689
+NINO_SITE = https://github.com/evanlin96069/nino
+NINO_SITE_METHOD = git
+
+NINO_LICENSE = BSD-2-Clause
+NINO_LICENSE_FILES = LICENSE
+
+# Uses CMake as its build system
+NINO_SUPPORTS_IN_SOURCE_BUILD = NO
+
+# Pre-generate resources/bundle.h using a host-built bundler and disable
+# the in-tree bundler in CMake when cross-compiling.
+# This avoids trying to run a target binary on the host during the build.
+define NINO_GENERATE_BUNDLE
+	$(HOSTCC) -O2 -o $(@D)/bundler $(@D)/resources/bundler.c
+	$(@D)/bundler $(@D)/resources/bundle.h \
+		$(@D)/resources/syntax/c.json \
+		$(@D)/resources/syntax/cpp.json \
+		$(@D)/resources/syntax/java.json \
+		$(@D)/resources/syntax/json.json \
+		$(@D)/resources/syntax/make.json \
+		$(@D)/resources/syntax/python.json \
+		$(@D)/resources/syntax/rust.json \
+		$(@D)/resources/syntax/zig.json
+	# Drop bundler targets and custom_command; keep using the pre-generated bundle.h
+	sed -i \
+		-e '/^add_executable(bundler /,/^)/d' \
+		-e '/^set *(BUNDLER_BIN/d' \
+		-e '/^add_custom_command[[:space:]]*(/,/^)/d' \
+		$(@D)/CMakeLists.txt
+endef
+NINO_PRE_CONFIGURE_HOOKS += NINO_GENERATE_BUNDLE
+
+$(eval $(cmake-package))
+
