bc117a971f1da6e93086aeb7a9eca6a763729983
Paul Philippov <paul@themactep.com>
2025-10-26 04:49:57 -0400

    prudynt-t: Fix T20 compilation - add compile-time guards for set_hue
    Update to commit 00b8ef6 which fixes compilation for T20 platforms.
    
    The IMP_ISP_Tuning_SetBcshHue function doesn't exist on T10/T20/T21/T30.
    Added compile-time guards to prevent compilation errors on platforms
    that don't have the function.
    
    Platforms with hue support:
    - T23, T31, C100: IMP_ISP_Tuning_SetBcshHue(val)
    - T40, T41: IMP_ISP_Tuning_SetBcshHue(IMPVI_MAIN, &val)
    - T10, T20, T21, T30: Function doesn't exist (returns 0)

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = t40-hal-support
-PRUDYNT_T_VERSION = f317949b225667f3160ede404dcfb63ef2a8d9a4
+PRUDYNT_T_VERSION = 00b8ef62745f9f357984fe430dc52bac8f2515a5
 
 PRUDYNT_T_GIT_SUBMODULES = YES
 
