cf86e4c2c2033500dd141867f4b7c2b3b9345533
Paul Philippov <paul@themactep.com>
2025-10-26 03:55:18 -0400

    prudynt-t: Fix capability initialization nesting
    Update to commit b3648d2 which fixes the has_framesource_chn_rotate
    capability initialization.
    
    The capability was incorrectly nested inside the OSD capability #else
    block. Fixed the #endif nesting so it's properly initialized for all
    platforms.
    
    This eliminates the 'missing initializer' compiler warning.

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = t40-hal-support
-PRUDYNT_T_VERSION = dffe537b044657827f5162fd57942604d110c10d
+PRUDYNT_T_VERSION = b3648d2fba7e985ad1c5cb8b87dbd749f2a0a49a
 
 PRUDYNT_T_GIT_SUBMODULES = YES
 
