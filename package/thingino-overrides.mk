601ca971956cc80edf9d5774ad6e0b9b9017c434
Paul Philippov <paul@themactep.com>
2025-12-09 02:58:25 -0500

    live555: convert to a virtual override package

--- package/thingino-overrides.mk
+++ package/thingino-overrides.mk
@@ -12,5 +12,7 @@
 # aggregated file.
 -include $(BR2_EXTERNAL)/local.mk
 -include $(CONFIG_DIR)/local.mk
+
 include $(BR2_EXTERNAL)/package/thingino-freetype/freetype-override.mk
+include $(BR2_EXTERNAL)/package/thingino-live555/live555-override.mk
 include $(BR2_EXTERNAL)/package/thingino-mxml/mxml-override.mk
