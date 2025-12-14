2786ca1f6d71d87d48bb46d00e1f28ecff45edee
Paul Philippov <paul@themactep.com>
2025-12-14 07:47:52 -0500

    faac: bump. support meson build system. make drm lib optional.

--- package/faac/faac.mk
+++ package/faac/faac.mk
@@ -1,56 +1,13 @@
 FAAC_SITE_METHOD = git
 FAAC_SITE = https://github.com/knik0/faac
 FAAC_SITE_BRANCH = master
-FAAC_VERSION = cb10c59666a3631547a9037c1576a34d8ca52df1
+FAAC_VERSION = 05f85a53d56f4c563524c5ffc2ebb2dd98fa7180
 
 FAAC_LICENSE = MPEG-4-Reference-Code, LGPL-2.1+
 FAAC_LICENSE_FILES = COPYING
 
-FAAC_INSTALL_STAGING = YES
-FAAC_INSTALL_TARGET = YES
+FAAC_DEPENDENCIES = host-pkgconf
 
-FAAC_AUTORECONF = YES
-FAAC_DEPENDENCIES += host-pkgconf host-libtool
+FAAC_CONF_OPTS = $(if $(BR2_PACKAGE_FAAC_ENABLE_DRM),-Denable_drm=true,-Denable_drm=false)
 
-FAAC_CONF_OPTS = --prefix=/usr --enable-shared --disable-static
-
-FAAC_LDFLAGS = $(TARGET_LDFLAGS) -z max-page-size=0x1000
-FAAC_CONF_ENV = PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)" LDFLAGS="$(FAAC_LDFLAGS)"
-
-define FAAC_CONFIGURE_CMDS
-	(cd $(FAAC_SRCDIR) && rm -rf config.cache && \
-	$(TARGET_CONFIGURE_OPTS) \
-	$(TARGET_CONFIGURE_ARGS) \
-	$(FAAC_CONF_ENV) \
-	./configure \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
-		--target=$(GNU_TARGET_NAME) \
-		$(FAAC_CONF_OPTS) \
-	)
-endef
-
-define FAAC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libfaac/.libs/libfaac.so.0.0.0 \
-		$(TARGET_DIR)/usr/lib/libfaac.so.0.0.0
-
-	ln -sf libfaac.so.0.0.0 $(TARGET_DIR)/usr/lib/libfaac.so.0
-	ln -sf libfaac.so.0.0.0 $(TARGET_DIR)/usr/lib/libfaac.so
-endef
-
-define FAAC_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/include/faac.h \
-		$(STAGING_DIR)/usr/include/faac.h
-
-	$(INSTALL) -D -m 0644 $(@D)/include/faaccfg.h \
-		$(STAGING_DIR)/usr/include/faaccfg.h
-
-	$(INSTALL) -D -m 0755 $(@D)/libfaac/.libs/libfaac.so.0.0.0 \
-		$(STAGING_DIR)/usr/lib/libfaac.so.0.0.0
-
-	ln -sf libfaac.so.0.0.0 $(STAGING_DIR)/usr/lib/libfaac.so.0
-	ln -sf libfaac.so.0.0.0 $(STAGING_DIR)/usr/lib/libfaac.so
-endef
-
-$(eval $(autotools-package))
-$(eval $(host-autotools-package))
+$(eval $(meson-package))
