ab7eee93c8338273764f2ff5626d1caf8c227d4c
Paul Philippov <paul@themactep.com>
2026-01-01 10:56:51 -0500

    package/prudynt-t: update to b2560a8
    Update prudynt-t from 1e7471e to b2560a8
    
    Hash change: 1e7471e34b13bac0385a119080c7e124a700f63e -> b2560a82233fdf851275bfa6c27b45d551b4a5a9
    
    Changelog:
    
      45f7695: rework audio-only session handling for robust /mic stream
      7d1a6e6: do not make audio buffer to depend on video stream
      b2560a8: fixes of RPT packets flow

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 1e7471e34b13bac0385a119080c7e124a700f63e
+PRUDYNT_T_VERSION = b2560a82233fdf851275bfa6c27b45d551b4a5a9
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt-override.json
 
