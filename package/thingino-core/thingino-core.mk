84008deb6ec2168dec8ef8a88a9cb221190ed4fd
Paul Philippov <paul@themactep.com>
2026-01-15 11:10:40 -0500

    core: relocate and rename common thingino.json file, reduce package to only handling changes

--- package/thingino-core/thingino-core.mk
+++ package/thingino-core/thingino-core.mk
@@ -6,15 +6,16 @@ THINGINO_CORE_DEPENDENCIES = host-thingino-jct
 THINGINO_CORE_OUTPUT_FILE=$(TARGET_DIR)/etc/thingino.json
 
 define THINGINO_CORE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(THINGINO_CORE_PKGDIR)/files/thingino.json $(THINGINO_CORE_OUTPUT_FILE)
-
-	[ -f $(THINGINO_CORE_OUTPUT_FILE) ] || echo '{}' > $(THINGINO_CORE_OUTPUT_FILE)
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL)/configs/common.thingino.json \
+		$(THINGINO_CORE_OUTPUT_FILE)
 
 	CAMERA_CONFIG=$(BR2_EXTERNAL)/$(CAMERA_SUBDIR)/$(CAMERA)/thingino-camera.json; \
-	[ -f $$CAMERA_CONFIG ] && $(HOST_DIR)/bin/jct $(THINGINO_CORE_OUTPUT_FILE) import $$CAMERA_CONFIG || true
+	[ -f "$$CAMERA_CONFIG" ] && \
+		$(HOST_DIR)/bin/jct "$(THINGINO_CORE_OUTPUT_FILE)" import "$$CAMERA_CONFIG" || true
 
-	USER_CONFIG=$(BR2_EXTERNAL)/configs/thingino-local.json; \
-	[ -f $$USER_CONFIG ] && $(HOST_DIR)/bin/jct $(THINGINO_CORE_OUTPUT_FILE) import $$USER_CONFIG || true
+	USER_CONFIG=$(BR2_EXTERNAL)/configs/local.thingino.json; \
+	[ -f "$$USER_CONFIG" ] && \
+		$(HOST_DIR)/bin/jct "$(THINGINO_CORE_OUTPUT_FILE)" import "$$USER_CONFIG" || true
 
 	printf "thingino-core: generated %s\n" $(THINGINO_CORE_OUTPUT_FILE) 1>&2
 endef
