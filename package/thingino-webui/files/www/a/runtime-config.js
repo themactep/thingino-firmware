b5ef7f4b4425d297f7dc7dd21455696461ad9744
Paul Philippov <paul@themactep.com>
2026-01-21 15:08:18 -0500

    webui: more generic config stub

--- package/thingino-webui/files/www/a/runtime-config.js
+++ package/thingino-webui/files/www/a/runtime-config.js
@@ -1,10 +1,9 @@
-// generated at Sat Jan 17 00:22:23 2026
 window.thinginoUIConfig = window.thinginoUIConfig || {};
 window.thinginoUIConfig.footer = Object.assign({}, window.thinginoUIConfig.footer, {
-  host: "ing-cinnado-d1-f8a4",
-  buildInfo: "cinnado_d1_t31l_sc2336_atbm6031 · stable+751a149 · 2026-01-16 19:13:22 +0000"
+  host: "localhost",
+  buildInfo: "unknown"
 });
 window.thinginoUIConfig.device = Object.assign({}, window.thinginoUIConfig.device, {
-  timezone: "America/Toronto"
+  timezone: "GMT"
 });
 window.thinginoFooterConfig = window.thinginoUIConfig.footer;
