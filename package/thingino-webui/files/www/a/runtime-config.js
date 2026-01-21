e2a352013caf7751b94f2b850c1e1e10f5f8fed7
Paul Philippov <paul@themactep.com>
2026-01-21 15:19:31 -0500

    webui: remove config stub from the firmware, it's created on boot

--- package/thingino-webui/files/www/a/runtime-config.js
+++
@@ -1,9 +0,0 @@
-window.thinginoUIConfig = window.thinginoUIConfig || {};
-window.thinginoUIConfig.footer = Object.assign({}, window.thinginoUIConfig.footer, {
-  host: "localhost",
-  buildInfo: "unknown"
-});
-window.thinginoUIConfig.device = Object.assign({}, window.thinginoUIConfig.device, {
-  timezone: "GMT"
-});
-window.thinginoFooterConfig = window.thinginoUIConfig.footer;
