1927f724de49dcf0bc688e69b80ede81d62186be
Paul Philippov <paul@themactep.com>
2026-02-01 20:39:09 -0500

    mbedtls: expand certificate generator to support both der and pem formats.

--- package/mbedtls-certgen/files/mbedtls-certgen.c
+++ package/mbedtls-certgen/files/mbedtls-certgen.c
@@ -42,13 +42,14 @@
 #define DEFAULT_RSA_KEY_SIZE 2048
 
 static void usage(const char *prog) {
-    printf("Usage: %s -h hostname -c cert_file -k key_file [-d days] [-s key_size] [-t type]\n", prog);
+    printf("Usage: %s -h hostname -c cert_file -k key_file [-d days] [-s key_size] [-t type] [-f format]\n", prog);
     printf("  -h, --hostname   Hostname for certificate CN\n");
     printf("  -c, --cert       Output certificate file\n");
     printf("  -k, --key        Output private key file\n");
     printf("  -d, --days       Certificate validity in days (default: %d)\n", DEFAULT_DAYS);
     printf("  -s, --key-size   Key size - ECDSA: 256,384,521 RSA: 2048,3072,4096 (default: %d)\n", DEFAULT_KEY_SIZE);
     printf("  -t, --type       Key type: ecdsa or rsa (default: ecdsa)\n");
+    printf("  -f, --format     Output format: pem or der (default: pem)\n");
     printf("  --help           Show this help message\n");
 }
 
@@ -84,6 +85,37 @@ static int write_private_key_pem(mbedtls_pk_context *key, const char *key_file) 
     return 0;
 }
 
+static int write_private_key_der(mbedtls_pk_context *key, const char *key_file) {
+    FILE *fp;
+    unsigned char buf[4096];
+    int ret;
+
+    fp = fopen(key_file, "wb");
+    if (!fp) {
+        fprintf(stderr, "Error: Cannot open key file for writing: %s\n", key_file);
+        return -1;
+    }
+
+    ret = mbedtls_pk_write_key_der(key, buf, sizeof(buf));
+    if (ret < 0) {
+        char error_buf[100];
+        mbedtls_strerror(ret, error_buf, sizeof(error_buf));
+        fprintf(stderr, "Error writing private key DER: -0x%04x - %s\n", (unsigned int) -ret, error_buf);
+        fclose(fp);
+        return -1;
+    }
+
+    /* DER is written from the end of the buffer backwards */
+    if (fwrite(buf + sizeof(buf) - ret, 1, ret, fp) != (size_t)ret) {
+        fprintf(stderr, "Error writing key to file\n");
+        fclose(fp);
+        return -1;
+    }
+
+    fclose(fp);
+    return 0;
+}
+
 static int write_certificate_pem(mbedtls_x509write_cert *crt, const char *cert_file,
                                  mbedtls_ctr_drbg_context *ctr_drbg) {
     FILE *fp;
@@ -106,6 +138,38 @@ static int write_certificate_pem(mbedtls_x509write_cert *crt, const char *cert_f
     }
 
     if (fwrite(buf, 1, strlen((char*)buf), fp) != strlen((char*)buf)) {
+        fprintf(stderr, "Error writing certificate to file\n");
+        fclose(fp);
+        return -1;
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+static int write_certificate_der(mbedtls_x509write_cert *crt, const char *cert_file,
+                                 mbedtls_ctr_drbg_context *ctr_drbg) {
+    FILE *fp;
+    unsigned char buf[4096];
+    int ret;
+
+    fp = fopen(cert_file, "wb");
+    if (!fp) {
+        fprintf(stderr, "Error: Cannot open certificate file for writing: %s\n", cert_file);
+        return -1;
+    }
+
+    ret = mbedtls_x509write_crt_der(crt, buf, sizeof(buf), mbedtls_ctr_drbg_random, ctr_drbg);
+    if (ret < 0) {
+        char error_buf[100];
+        mbedtls_strerror(ret, error_buf, sizeof(error_buf));
+        fprintf(stderr, "Error writing certificate DER: -0x%04x - %s\n", (unsigned int) -ret, error_buf);
+        fclose(fp);
+        return -1;
+    }
+
+    /* DER is written from the end of the buffer backwards */
+    if (fwrite(buf + sizeof(buf) - ret, 1, ret, fp) != (size_t)ret) {
         fprintf(stderr, "Error writing certificate to file\n");
         fclose(fp);
         return -1;
@@ -182,7 +246,7 @@ static int generate_rsa_key(mbedtls_pk_context *key, int key_size, mbedtls_ctr_d
 }
 
 static int generate_certificate(const char *cert_file, const char *key_file,
-                               const char *hostname, int days, int key_size, const char *key_type) {
+                               const char *hostname, int days, int key_size, const char *key_type, const char *format) {
     mbedtls_pk_context key;
     mbedtls_x509write_cert crt;
     mbedtls_entropy_context entropy;
@@ -192,7 +256,7 @@ static int generate_certificate(const char *cert_file, const char *key_file,
     char subject_name[256];
     char serial_str[32];
 
-    printf("Generating %s key and certificate for hostname: %s\n", key_type, hostname);
+    printf("Generating %s key and certificate for hostname: %s (format: %s)\n", key_type, hostname, format);
 
     /* Initialize contexts */
     mbedtls_pk_init(&key);
@@ -223,7 +287,11 @@ static int generate_certificate(const char *cert_file, const char *key_file,
     }
 
     /* Write private key */
-    ret = write_private_key_pem(&key, key_file);
+    if (strcmp(format, "der") == 0) {
+        ret = write_private_key_der(&key, key_file);
+    } else {
+        ret = write_private_key_pem(&key, key_file);
+    }
     if (ret != 0) {
         goto cleanup;
     }
@@ -277,7 +345,11 @@ static int generate_certificate(const char *cert_file, const char *key_file,
 
     /* Write certificate */
     printf("Generating self-signed certificate...\n");
-    ret = write_certificate_pem(&crt, cert_file, &ctr_drbg);
+    if (strcmp(format, "der") == 0) {
+        ret = write_certificate_der(&crt, cert_file, &ctr_drbg);
+    } else {
+        ret = write_certificate_pem(&crt, cert_file, &ctr_drbg);
+    }
     if (ret != 0) {
         goto cleanup;
     }
@@ -301,6 +373,7 @@ int main(int argc, char *argv[]) {
     int days = DEFAULT_DAYS;
     int key_size = DEFAULT_KEY_SIZE;
     char *key_type = "ecdsa";
+    char *format = "pem";
     int opt;
 
     static struct option long_options[] = {
@@ -310,11 +383,12 @@ int main(int argc, char *argv[]) {
         {"days", required_argument, 0, 'd'},
         {"key-size", required_argument, 0, 's'},
         {"type", required_argument, 0, 't'},
+        {"format", required_argument, 0, 'f'},
         {"help", no_argument, 0, 0},
         {0, 0, 0, 0}
     };
 
-    while ((opt = getopt_long(argc, argv, "h:c:k:d:s:t:", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "h:c:k:d:s:t:f:", long_options, NULL)) != -1) {
         switch (opt) {
             case 'h':
                 hostname = optarg;
@@ -339,6 +413,13 @@ int main(int argc, char *argv[]) {
                 key_type = optarg;
                 if (strcmp(key_type, "ecdsa") != 0 && strcmp(key_type, "rsa") != 0) {
                     fprintf(stderr, "Error: Invalid key type: %s (must be 'ecdsa' or 'rsa')\n", key_type);
+                    return 1;
+                }
+                break;
+            case 'f':
+                format = optarg;
+                if (strcmp(format, "pem") != 0 && strcmp(format, "der") != 0) {
+                    fprintf(stderr, "Error: Invalid format: %s (must be 'pem' or 'der')\n", format);
                     return 1;
                 }
                 break;
@@ -367,7 +448,7 @@ int main(int argc, char *argv[]) {
 
 #ifdef HAVE_MBEDTLS
     /* Generate certificate and key */
-    if (generate_certificate(cert_file, key_file, hostname, days, key_size, key_type) != 0) {
+    if (generate_certificate(cert_file, key_file, hostname, days, key_size, key_type, format) != 0) {
         fprintf(stderr, "Failed to generate certificate and key\n");
         return 1;
     }
