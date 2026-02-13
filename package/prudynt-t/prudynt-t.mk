ad1f46fbaa956d42e3340bf21b2db38be0f81850
Paul Philippov <paul@themactep.com>
2026-02-13 13:08:58 -0500

    package/prudynt-t: update to 3dbe563
    Update prudynt-t from 3c8e835 to 3dbe563
    
    Hash change: 3c8e8350cffbf5f66856e1a8902fbc22711f8895 -> 3dbe563c9614cbb3dd813e296fd716501062a3af
    
    Changelog:
    
      835fca2: prudunt: verbose logging aroung AAC encoder
      f0c89a9: prudynt: remove excessive logging in audio buffer
      50b0d0e: prudynt: wait for sensor initialization to complete
      d9d0e00: adjust .gitignore
      84fa818: init daynight algo in an undetermined mode, do not assume day or night
      3f400cc: try to recover failing AAC encoding
      90ac6cc: implement modular design. disable unwanted components as needed.
      3dbe563: extend hal to add missing prototypes

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 3c8e8350cffbf5f66856e1a8902fbc22711f8895
+PRUDYNT_T_VERSION = 3dbe563c9614cbb3dd813e296fd716501062a3af
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt-override.json
 
