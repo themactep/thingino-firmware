5cc3bb2225ad520ff4ee5774ed4b0c928f4fb6c0
Paul Philippov <paul@themactep.com>
2026-02-19 06:54:49 -0500

    add virtual thingino-ssl package

---
+++ package/thingino-ssl/thingino-ssl.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# thingino-ssl
+#
+################################################################################
+
+# Virtual package - no source
+THINGINO_SSL_LICENSE = N/A
+
+# This is a virtual package that doesn't build anything
+# It exists solely to provide a common dependency point for SSL/TLS libraries
+
+$(eval $(virtual-package))
