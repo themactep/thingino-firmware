9a537f00fb949aeeafd665bf541a01cc48cafa04
Paul Philippov <paul@themactep.com>
2026-02-19 06:51:36 -0500

    makefile: remove deprecated modules from configuration builder

--- board.mk
+++ board.mk
@@ -60,33 +60,19 @@ ifeq ($(CAMERA_CONFIG_REAL),)
 $(error * Camera config file not found: $(BR2_EXTERNAL)/$(CAMERA_CONFIG). Please check if the profile still exists or remove the BUILD_MEMO file: $(BUILD_MEMO))
 endif
 
-MODULE_CONFIG = $(shell awk '/MODULE:/ {$$1=$$1;gsub(/^.+:\s*/,"");print}' $(CAMERA_CONFIG_REAL))
-ifeq ($(MODULE_CONFIG),)
-MODULE_CONFIG_REAL = $(CAMERA_CONFIG_REAL)
-else
-MODULE_CONFIG_REAL = $(shell realpath "$(BR2_EXTERNAL)/configs/modules/$(MODULE_CONFIG)_defconfig")
-endif
-$(info MODULE_CONFIG = $(MODULE_CONFIG))
-
-#$(info * restore CAMERA for CAMERA_CONFIG)
-#CAMERA = $(shell basename "$(CAMERA_CONFIG_REAL)")
-
 export CAMERA
 $(info CAMERA = $(CAMERA))
 
 # read camera config file
 include $(CAMERA_CONFIG_REAL)
 
-# read module config file
-include $(MODULE_CONFIG_REAL)
-
 else
+
 # Board selection skipped for exempted targets
 # Set minimal required variables to prevent errors
 CAMERA_CONFIG :=
 CAMERA_CONFIG_REAL :=
-MODULE_CONFIG :=
-MODULE_CONFIG_REAL :=
 CAMERA :=
 $(info Board selection skipped for target: $(CURRENT_TARGETS))
+
 endif
