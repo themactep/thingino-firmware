f90dfaf772c36fd808a9f80bac4e3dfd74040299
Nils Schimmelmann <nschimme@gmail.com>
2026-02-27 15:30:57 +0000

    faac: bump. switch to single precision and use upstreamed DRM patch

--- package/faac/faac.mk
+++ package/faac/faac.mk
@@ -1,7 +1,7 @@
 FAAC_SITE_METHOD = git
 FAAC_SITE = https://github.com/knik0/faac
 FAAC_SITE_BRANCH = master
-FAAC_VERSION = 05f85a53d56f4c563524c5ffc2ebb2dd98fa7180
+FAAC_VERSION = 64fc54142912088eecabaa159339768eca30f3d6
 
 FAAC_LICENSE = MPEG-4-Reference-Code, LGPL-2.1+
 FAAC_LICENSE_FILES = COPYING
@@ -11,7 +11,9 @@ FAAC_DEPENDENCIES = host-pkgconf
 FAAC_INSTALL_STAGING = YES
 FAAC_INSTALL_TARGET = YES
 
-FAAC_CONF_OPTS = $(if $(BR2_PACKAGE_FAAC_ENABLE_DRM),-Denable_drm=true,-Denable_drm=false)
+FAAC_CONF_OPTS = \
+	$(if $(BR2_PACKAGE_FAAC_ENABLE_DRM),-Ddrm=true,-Ddrm=false) \
+	-Dfloating-point=single
 
 ifneq ($(BR2_PACKAGE_FAAC_INSTALL_BIN),y)
 define FAAC_REMOVE_TARGET_BINARY
