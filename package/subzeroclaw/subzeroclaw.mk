dd3fba14aaed669b25b17a5304b9cd22a8f84869
Paul Philippov <paul@themactep.com>
2026-03-02 04:42:08 -0500

    add subzeroclaw package

---
+++ package/subzeroclaw/subzeroclaw.mk
@@ -0,0 +1,35 @@
+################################################################################
+# subzeroclaw
+################################################################################
+
+SUBZEROCLAW_VERSION = 98849089aeb0e04ed47bcad0c04606d33846f2e0
+SUBZEROCLAW_SITE = https://github.com/themactep/subzeroclaw
+SUBZEROCLAW_SITE_METHOD = git
+
+SUBZEROCLAW_LICENSE = MIT
+SUBZEROCLAW_LICENSE_FILES = LICENSE
+
+SUBZEROCLAW_DEPENDENCIES = cjson thingino-libcurl
+
+SUBZEROCLAW_CFLAGS += \
+	-std=c11 -Wall -Wextra -O2 -D_GNU_SOURCE \
+	-I$(STAGING_DIR)/usr/include
+
+SUBZEROCLAW_LDFLAGS += \
+	-L$(STAGING_DIR)/usr/lib \
+	-lcjson -lcurl -lm
+
+define SUBZEROCLAW_BUILD_CMDS
+	$(TARGET_CC) $(TARGET_CFLAGS) $(SUBZEROCLAW_CFLAGS) \
+		-o $(@D)/subzeroclaw $(@D)/src/subzeroclaw.c \
+		$(TARGET_LDFLAGS) $(SUBZEROCLAW_LDFLAGS)
+endef
+
+define SUBZEROCLAW_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/subzeroclaw/skills
+	$(INSTALL) -D -m 0755 $(@D)/subzeroclaw $(TARGET_DIR)/usr/bin/subzeroclaw
+	$(INSTALL) -D -m 0644 $(SUBZEROCLAW_PKGDIR)/files/config $(TARGET_DIR)/etc/subzeroclaw/config
+	$(INSTALL) -D -m 0644 $(SUBZEROCLAW_PKGDIR)/files/system.md $(TARGET_DIR)/etc/subzeroclaw/skills/system.md
+endef
+
+$(eval $(generic-package))
