2414ab93c713cbeb4221ecac01b2d4881be42c92
Paul Philippov <paul@themactep.com>
2026-03-04 19:41:23 -0500

    thingino-webserver: add busybox httpd config fragment
    When BR2_PACKAGE_THINGINO_WEBSERVER_BUSYBOX is selected, inject a BusyBox
    kconfig fragment enabling all required httpd features:
    CGI, basic auth, MD5 auth, ranges, proxy, gzip, etag, last-modified,
    date, ACL, URL encoding, error pages, and script interpreter support.
    
    Fragment is appended via BUSYBOX_KCONFIG_FRAGMENT_FILES in
    busybox-httpd-override.mk, included from thingino-overrides.mk.

--- package/thingino-overrides.mk
+++ package/thingino-overrides.mk
@@ -13,6 +13,7 @@
 -include $(BR2_EXTERNAL)/local.mk
 -include $(CONFIG_DIR)/local.mk
 
+include $(BR2_EXTERNAL)/package/thingino-webserver/busybox-httpd-override.mk
 include $(BR2_EXTERNAL)/package/thingino-freetype/freetype-override.mk
 include $(BR2_EXTERNAL)/package/thingino-libwebsockets/libwebsockets-override.mk
 include $(BR2_EXTERNAL)/package/thingino-live555/live555-override.mk
