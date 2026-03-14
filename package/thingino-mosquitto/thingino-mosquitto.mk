78e1d41bb3aa75b77fb6d2491d38947a89ed5c24
Paul Philippov <paul@themactep.com>
2026-03-14 01:13:04 -0400

    mosquitto: bump to 2.1.2

--- package/thingino-mosquitto/thingino-mosquitto.mk
+++ package/thingino-mosquitto/thingino-mosquitto.mk
@@ -4,10 +4,6 @@
 #
 ################################################################################
 
-# Stick to the Buildroot mosquitto implementation but pin our preferred source.
-override MOSQUITTO_VERSION = 2.0.22
-override MOSQUITTO_SITE = https://sources.buildroot.net/mosquitto
-
 # Ensure our target/host wrappers trigger the upstream package.
 THINGINO_MOSQUITTO_DEPENDENCIES = mosquitto
 HOST_THINGINO_MOSQUITTO_DEPENDENCIES = host-mosquitto
