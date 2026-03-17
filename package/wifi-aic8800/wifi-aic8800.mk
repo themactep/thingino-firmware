7473e46b132ba66c5a43b31654a8ff37bfef8005
Paul Philippov <paul@themactep.com>
2026-03-17 13:23:22 -0400

    wifi aic8800: slim down the driver removing nonessential firmware

--- package/wifi-aic8800/wifi-aic8800.mk
+++ package/wifi-aic8800/wifi-aic8800.mk
@@ -8,13 +8,30 @@ WIFI_AIC8800_LICENSE = GPL-2.0
 AIC8800_MODULE_NAME = aic8800_fdrv
 AIC8800_MODULE_OPTS =
 
+WIFI_AIC8800_MODULE_MAKE_OPTS = \
+	CONFIG_USE_WIRELESS_EXT=n \
+	CONFIG_RWNX_RADAR=n \
+	INSTALL_MOD_STRIP=1
+
+ifeq ($(BR2_PACKAGE_WIFI_AIC8800_USB_8800),y)
+WIFI_AIC8800_MODULE_MAKE_OPTS += CONFIG_AIC_CHIP_8800=y
+endif
+
+ifeq ($(BR2_PACKAGE_WIFI_AIC8800_USB_8800D80),y)
+WIFI_AIC8800_MODULE_MAKE_OPTS += CONFIG_AIC_CHIP_8800D80=y
+endif
+
+ifeq ($(BR2_PACKAGE_WIFI_AIC8800_USB_8800D80X2),y)
+WIFI_AIC8800_MODULE_MAKE_OPTS += CONFIG_AIC_CHIP_8800D80X2=y
+endif
+
+ifeq ($(BR2_PACKAGE_WIFI_AIC8800_USB_8800DC),y)
+WIFI_AIC8800_MODULE_MAKE_OPTS += CONFIG_AIC_CHIP_8800DC=y
+endif
+
 define WIFI_AIC8800_LINUX_CONFIG_FIXUPS
 	$(call KCONFIG_ENABLE_OPT,CONFIG_WLAN)
 	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
-	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS_EXT)
-	$(call KCONFIG_ENABLE_OPT,CONFIG_WEXT_CORE)
-	$(call KCONFIG_ENABLE_OPT,CONFIG_WEXT_PROC)
-	$(call KCONFIG_ENABLE_OPT,CONFIG_WEXT_PRIV)
 	$(call KCONFIG_SET_OPT,CONFIG_CFG80211,y)
 	$(call KCONFIG_SET_OPT,CONFIG_MAC80211,y)
 	$(call KCONFIG_ENABLE_OPT,CONFIG_MAC80211_RC_MINSTREL)
@@ -71,15 +88,19 @@ WIFI_AIC8800_USB_8800_FW_FILES = \
 	m2d_ota.bin
 
 WIFI_AIC8800_USB_8800D80_FW_DIR = USB/driver_fw/fw/aic8800D80
-# Minimal subset required for normal operation (see aic_compat_8800d80.c)
+# Minimal subset for on H-variant devices only
 WIFI_AIC8800_USB_8800D80_FW_FILES = \
 	aic_userconfig_8800d80.txt \
 	fmacfw_8800d80_h_u02.bin \
-	fmacfw_8800d80_u02.bin \
 	fw_adid_8800d80_u02.bin \
 	fw_patch_8800d80_u02.bin \
 	fw_patch_8800d80_u02_ext0.bin \
 	fw_patch_table_8800d80_u02.bin
+# for non-H variants
+ifeq ($(BR2_PACKAGE_WIFI_AIC8800_USB_8800D80_H_FIRMWARE_ONLY),)
+WIFI_AIC8800_USB_8800D80_FW_FILES += \
+	fmacfw_8800d80_h_u02.bin
+endif
 
 WIFI_AIC8800_USB_8800D80X2_FW_DIR = USB/driver_fw/fw/aic8800D80X2
 WIFI_AIC8800_USB_8800D80X2_FW_FILES = \
@@ -93,25 +114,26 @@ WIFI_AIC8800_USB_8800D80X2_FW_FILES = \
 
 WIFI_AIC8800_USB_8800DC_FW_DIR = USB/driver_fw/fw/aic8800DC
 WIFI_AIC8800_USB_8800DC_FW_FILES = \
-	aic_powerlimit_8800dc.txt \
-	aic_powerlimit_8800dw.txt \
 	aic_userconfig_8800dc.txt \
-	aic_userconfig_8800dw.txt \
 	fmacfw_calib_8800dc_h_u02.bin \
-	fmacfw_calib_8800dc_u02.bin \
 	fmacfw_patch_8800dc_h_u02.bin \
-	fmacfw_patch_8800dc_ipc_u02.bin \
-	fmacfw_patch_8800dc_u02.bin \
-	fmacfw_patch_tbl_8800dc_h_u02.bin \
-	fmacfw_patch_tbl_8800dc_ipc_u02.bin \
-	fmacfw_patch_tbl_8800dc_u02.bin \
-	fw_adid_8800dc_u02.bin \
-	fw_adid_8800dc_u02h.bin \
-	fw_patch_8800dc_u02.bin \
-	fw_patch_8800dc_u02h.bin \
-	fw_patch_table_8800dc_u02.bin \
-	fw_patch_table_8800dc_u02h.bin \
-	lmacfw_rf_8800dc.bin
+	fmacfw_patch_tbl_8800dc_h_u02.bin
+
+#	aic_powerlimit_8800dc.txt \
+#	aic_powerlimit_8800dw.txt \
+#	aic_userconfig_8800dw.txt \
+#	fmacfw_calib_8800dc_u02.bin \
+#	fmacfw_patch_8800dc_ipc_u02.bin \
+#	fmacfw_patch_8800dc_u02.bin \
+#	fmacfw_patch_tbl_8800dc_ipc_u02.bin \
+#	fmacfw_patch_tbl_8800dc_u02.bin \
+#	fw_adid_8800dc_u02.bin \
+#	fw_adid_8800dc_u02h.bin \
+#	fw_patch_8800dc_u02.bin \
+#	fw_patch_8800dc_u02h.bin \
+#	fw_patch_table_8800dc_u02.bin \
+#	fw_patch_table_8800dc_u02h.bin \
+#	lmacfw_rf_8800dc.bin
 
 WIFI_AIC8800_SDIO_8800_FW_DIR = SDIO/driver_fw/fw/aic8800
 WIFI_AIC8800_SDIO_8800_FW_FILES = \
