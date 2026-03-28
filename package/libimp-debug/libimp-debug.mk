455d882488ac3d062c8e067d805e07fea7670184
gtxaspec <gtxent@gmail.com>
2026-03-27 19:13:19 -0700

    libimp-debug: install nodbg.so, preload on RAD for multi-process fix
    Install libimp-nodbg.so alongside libimp-debug. When present, S31raptor
    preloads it on RAD to prevent a competing IMP debug shm_thread, which
    allows libimp-debug commands to work reliably with raptor.

--- package/libimp-debug/libimp-debug.mk
+++ package/libimp-debug/libimp-debug.mk
@@ -1,14 +1,15 @@
 LIBIMP_DEBUG_SITE_METHOD = git
 LIBIMP_DEBUG_SITE = https://github.com/gtxaspec/libimp-debug
 LIBIMP_DEBUG_SITE_BRANCH = main
-LIBIMP_DEBUG_VERSION = 026a6549e9a0026536fa87dbb9b33d5f50af4293
+LIBIMP_DEBUG_VERSION = 44042d4
 
 define LIBIMP_DEBUG_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) libimp-debug
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
 define LIBIMP_DEBUG_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/libimp-debug $(TARGET_DIR)/usr/bin/libimp-debug
+	$(INSTALL) -D -m 0755 $(@D)/libimp-nodbg.so $(TARGET_DIR)/usr/lib/libimp-nodbg.so
 endef
 
 $(eval $(generic-package))
