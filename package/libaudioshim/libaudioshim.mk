87c8411223ff8875e9e2a5a97711ae42e637d22b
Paul Philippov <paul@themactep.com>
2026-04-10 03:24:40 -0400

    add audioshim package

---
+++ package/libaudioshim/libaudioshim.mk
@@ -0,0 +1,22 @@
+LIBAUDIOSHIM_SITE_METHOD = git
+LIBAUDIOSHIM_SITE = https://github.com/gtxaspec/libaudioshim
+LIBAUDIOSHIM_SITE_BRANCH = master
+LIBAUDIOSHIM_VERSION = 8e6b413d351e2a32cfe83b226b968b421fb8aee1
+LIBAUDIOSHIM_INSTALL_STAGING = YES
+
+LIBAUDIOSHIM_LICENSE = GPL-2.0
+LIBAUDIOSHIM_LICENSE_FILES = LICENSE
+
+define LIBAUDIOSHIM_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) CROSS_COMPILE=$(TARGET_CROSS)
+endef
+
+define LIBAUDIOSHIM_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libaudioshim.so $(STAGING_DIR)/usr/lib/libaudioshim.so
+endef
+
+define LIBAUDIOSHIM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libaudioshim.so $(TARGET_DIR)/usr/lib/libaudioshim.so
+endef
+
+$(eval $(generic-package))
