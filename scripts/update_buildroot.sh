20ec1694eec45f7ba74b6f8d162d87b325cab3a7
Paul Philippov <paul@themactep.com>
2026-04-23 10:30:30 -0400

    Revert "update buildroot to the last known stable version"
    This reverts commit 251d8d1d7d5503a3917161e5d8da3ce232facb2a.

--- scripts/update_buildroot.sh
+++ scripts/update_buildroot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Update buildroot submodule to a specific version or latest stable release
+# Update buildroot submodule to the latest version
 
 set -eu
 
@@ -12,25 +12,8 @@ if [ -n "${1:-}" ]; then
     fi
     commit_message="Update buildroot to version $1"
 else
-    git -C "$buildroot_path" fetch --tags --quiet origin
-    latest_stable_release="$(
-        git -C "$buildroot_path" tag -l \
-            | grep -E '^[0-9]{4}\.[0-9]{2}(\.[0-9]+)?$' \
-            | sort -V \
-            | tail -n 1
-    )"
-
-    if [ -z "$latest_stable_release" ]; then
-        echo "Failed to resolve latest stable buildroot release tag"
-        exit 1
-    fi
-
-    if ! git -C "$buildroot_path" checkout "$latest_stable_release"; then
-        echo "Failed to checkout $latest_stable_release"
-        exit 1
-    fi
-
-    commit_message="Update buildroot to latest stable release $latest_stable_release"
+    git submodule update --remote "$buildroot_path"
+    commit_message="Update buildroot to latest version"
 fi
 
 git add "$buildroot_path"
