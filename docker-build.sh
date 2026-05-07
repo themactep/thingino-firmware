2435d0039c3638c7952dcf738d437084f6a24aec
Lukáš Mojžíš <lukas.mojzis@grenames.cz>
2026-05-07 12:50:57 +0200

    Docker Build: silence --format nags

--- docker-build.sh
+++ docker-build.sh
@@ -83,7 +83,7 @@ fi
 DOCKER_IMAGE="thingino-builder"
 DOCKER_TAG="latest"
 
-if ! $CONTAINER_ENGINE images | grep -q "$DOCKER_IMAGE.*$DOCKER_TAG"; then
+if ! $CONTAINER_ENGINE images --format=table | grep -q "$DOCKER_IMAGE.*$DOCKER_TAG"; then
     print_info "Building container image..."
     run_makefile_docker docker-build CONTAINER_ENGINE="$CONTAINER_ENGINE"
     print_success "Container image built"
