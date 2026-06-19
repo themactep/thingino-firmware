a3ee74310a89b70a2753820776edc561274b5502
Paul Philippov <paul@themactep.com>
2026-06-19 13:19:04 -0400

    package/thingino-dfu: update to v1.1.1
    Update thingino-dfu from v1.1.0 to v1.1.1
    
    Hash change: v1.1.0 -> v1.1.1
    
    (No changelog available)

--- package/thingino-dfu/thingino-dfu.mk
+++ package/thingino-dfu/thingino-dfu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-THINGINO_DFU_VERSION = v1.1.0
+THINGINO_DFU_VERSION = v1.1.1
 THINGINO_DFU_SITE = $(call github,wltechblog,thingino-dfu,$(THINGINO_DFU_VERSION))
 
 THINGINO_DFU_LICENSE = GPL-2.0
