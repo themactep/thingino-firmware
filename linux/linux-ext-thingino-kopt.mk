e22cb8607bcf71f87874b924a8439948c786a960
xu6ao2 <xu6ao2@outlook.com>
2026-07-13 03:08:02 -0700

    Add dts and Experimental config for 360 k7ts t41nq.
    note:
    1.The led_g is currently not working for all kernel 4.4 cameras.
    2.The external speaker works ,but I didn't find the disable/enable pin.
    3.The original firmware have gpio 57 and 61 as input, 82 output low, I don't know what are they.
    4.Motors need previous patch in ingenic-sdk to work.

--- linux/linux-ext-thingino-kopt.mk
+++ linux/linux-ext-thingino-kopt.mk
@@ -10,6 +10,8 @@ THINGINO_DTS_MAPPINGS = \
 	WYZEC3P|wyze_cam3pro_t40xp|shark \
 	EUFYT8416|eufy_t8416_t40xp|shark \
 	A1_SMART_NVR|smart_nvr_a1n_eth|tucana \
+	360K7TS|360_k7ts_t41nq_eth|marmot \
+	HUGOLOGE5P|hugolog_e5p_t41lq|marmot \
 	IGETC5PT|iget_c5pt_t41lq|marmot \
 	WYZEV4|wyze_cam4_t41nq|marmot \
 	WYZEPANV4|wyze_panv4_t32nq|goat
