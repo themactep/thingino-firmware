ebf9bedd0c07d0004edd4c384d399c595e941b44
gtxaspec <gtxent@gmail.com>
2026-07-15 08:35:01 -0700

    thingino-raptor: stage rss_sei + monocypher headers, install rverify
    raptor-common now ships rss_sei.h and the vendored monocypher headers
    needed by the SEI timecode and recording-signing code; rverify is
    built and installed alongside rmr for on-device verification.

--- package/thingino-raptor-common/thingino-raptor-common.mk
+++ package/thingino-raptor-common/thingino-raptor-common.mk
@@ -11,8 +11,12 @@ endef
 define THINGINO_RAPTOR_COMMON_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/librss_common.so \
 		$(STAGING_DIR)/usr/lib/librss_common.so
-	for h in rss_common.h rss_net.h rss_http.h rss_tls.h rss_ts.h cJSON.h; do \
+	for h in rss_common.h rss_net.h rss_http.h rss_tls.h rss_ts.h rss_sei.h cJSON.h; do \
 		$(INSTALL) -D -m 0644 $(@D)/include/$$h \
+			$(STAGING_DIR)/usr/include/$$h; \
+	done
+	for h in monocypher.h monocypher-ed25519.h; do \
+		$(INSTALL) -D -m 0644 $(@D)/third_party/monocypher/$$h \
 			$(STAGING_DIR)/usr/include/$$h; \
 	done
 	$(INSTALL) -D -m 0644 $(@D)/src/rss_tls.c \
