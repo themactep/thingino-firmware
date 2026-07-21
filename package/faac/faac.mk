dbda8e755a967f7d173807bdfe96c42ccd423008
Paul Philippov <paul@themactep.com>
2026-07-21 13:41:23 -0400

    packages: update faac to 2.0, libsrt to 1.5.6, libhelix-{aac,mp3} to 2.4.2
    faac 2.0 switches to meson build system and new faac_encoder API.
    libsrt and libhelix hash files updated to match version bumps in .mk.
    
    Signed-off-by: Paul Philippov <paul@themactep.com>

--- package/faac/faac.mk
+++ package/faac/faac.mk
@@ -1,4 +1,4 @@
-FAAC_VERSION = 1.50
+FAAC_VERSION = 2.0
 FAAC_SITE = $(call github,knik0,faac,faac-$(FAAC_VERSION))
 
 FAAC_LICENSE = MPEG-4-Reference-Code, LGPL-2.1+
@@ -7,81 +7,15 @@ FAAC_LICENSE_FILES = COPYING
 FAAC_INSTALL_STAGING = YES
 FAAC_INSTALL_TARGET = YES
 
-FAAC_CFLAGS = $(TARGET_CFLAGS) -ffast-math -DHAVE_CONFIG_H \
-	-I$(@D) -I$(@D)/include -fPIC
+FAAC_CFLAGS = $(TARGET_CFLAGS) -ffast-math
 
-FAAC_LIBFAAC_SRCS = bitstream.c blockswitch.c channels.c cpu_compute.c \
-	fft.c filtbank.c frame.c huff2.c huffdata.c \
-	quantize.c stereo.c tns.c util.c
-
-define FAAC_CONFIGURE_CMDS
-	printf '%s\n' \
-		'#define PACKAGE "faac"' \
-		'#define PACKAGE_VERSION "1.50"' \
-		'#define HAVE_GETOPT_H 1' \
-		'#define HAVE_STDINT_H 1' \
-		'#define HAVE_SYS_TIME_H 1' \
-		'#define HAVE_SYS_TYPES_H 1' \
-		'#define HAVE_STRCASECMP 1' \
-		'#define FAAC_PRECISION_SINGLE 1' \
-		'#define MAX_CHANNELS 2' \
-		> $(@D)/config.h
-endef
-
-define FAAC_BUILD_CMDS
-	for f in $(FAAC_LIBFAAC_SRCS); do \
-		$(TARGET_CC) $(FAAC_CFLAGS) -c \
-			$(@D)/libfaac/$$f -o $(@D)/libfaac/$${f%.c}.o || exit 1; \
-	done
-	$(TARGET_AR) rcs $(@D)/libfaac/libfaac.a \
-		$(addprefix $(@D)/libfaac/,$(FAAC_LIBFAAC_SRCS:.c=.o))
-	$(TARGET_CC) -shared -Wl,-soname,libfaac.so \
-		$(addprefix $(@D)/libfaac/,$(FAAC_LIBFAAC_SRCS:.c=.o)) \
-		$(TARGET_LDFLAGS) -lm -o $(@D)/libfaac/libfaac.so
-	$(FAAC_BUILD_FRONTEND)
-endef
-
-define FAAC_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/include/faac.h $(STAGING_DIR)/usr/include/faac.h
-	$(INSTALL) -D -m 0644 $(@D)/include/faaccfg.h $(STAGING_DIR)/usr/include/faaccfg.h
-	$(INSTALL) -D -m 0644 $(@D)/libfaac/libfaac.a $(STAGING_DIR)/usr/lib/libfaac.a
-	$(INSTALL) -D -m 0755 $(@D)/libfaac/libfaac.so $(STAGING_DIR)/usr/lib/libfaac.so
-	# Generate faac.pc inline. Upstream removed libfaac/faac.pc.in in
-	# knik0/faac commit 9ed7f23 ("remove obsolete pkg-config template"),
-	# so we can't sed a template that is not there. Mirror the content of
-	# the old template with our fixed staging paths.
-	mkdir -p $(STAGING_DIR)/usr/lib/pkgconfig
-	printf '%s\n' \
-		'prefix=/usr' \
-		'exec_prefix=/usr' \
-		'libdir=/usr/lib' \
-		'includedir=/usr/include' \
-		'' \
-		'Name: FAAC' \
-		'Description: Freeware Advanced Audio Coder' \
-		'Version: 1.50' \
-		'Libs: -L$${libdir} -lfaac' \
-		'Libs.private: -lm' \
-		'Cflags: -I$${includedir}' \
-		> $(STAGING_DIR)/usr/lib/pkgconfig/faac.pc
-endef
-
-define FAAC_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libfaac/libfaac.so $(TARGET_DIR)/usr/lib/libfaac.so
-endef
+FAAC_CONF_OPTS = \
+	-Dmax-channels=2
 
 ifeq ($(BR2_PACKAGE_FAAC_INSTALL_BIN),y)
-define FAAC_BUILD_FRONTEND
-	$(TARGET_CC) $(FAAC_CFLAGS) $(TARGET_LDFLAGS) \
-		$(@D)/frontend/main.c $(@D)/frontend/input.c $(@D)/frontend/mp4write.c \
-		-I$(@D)/frontend -L$(@D)/libfaac -lfaac -lm \
-		-o $(@D)/frontend/faac
-endef
-
-define FAAC_INSTALL_FRONTEND_TARGET
-	$(INSTALL) -D -m 0755 $(@D)/frontend/faac $(TARGET_DIR)/usr/bin/faac
-endef
-FAAC_POST_INSTALL_TARGET_HOOKS += FAAC_INSTALL_FRONTEND_TARGET
+FAAC_CONF_OPTS += -Dfrontend=true
+else
+FAAC_CONF_OPTS += -Dfrontend=false
 endif
 
-$(eval $(generic-package))
+$(eval $(meson-package))
