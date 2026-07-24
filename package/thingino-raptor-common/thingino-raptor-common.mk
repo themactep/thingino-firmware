b284ced8b211d1cba87e00273851a6e3d801becc
gtxaspec <gtxent@gmail.com>
2026-07-23 17:41:54 -0700

    raptor packages: install rss_aac.h to staging, bump pins to current mains
    rss_aac.h (raptor-common HE-AAC helper) was missing from the explicit
    staging header list, breaking rsd/rmr builds against current raptor.
    Pin bumps: raptor-common 75d83e8b, raptor 17b36386, raptor-hal
    7e62abed, compy 0649f6b7 (the May compy pin lacked the stateless
    digest nonce fix and the H265 MTU fix).

--- package/thingino-raptor-common/thingino-raptor-common.mk
+++ package/thingino-raptor-common/thingino-raptor-common.mk
@@ -1,4 +1,4 @@
-THINGINO_RAPTOR_COMMON_VERSION = 3dd6d6be8f1509a4f83587ad038deddb4c630960
+THINGINO_RAPTOR_COMMON_VERSION = 75d83e8bd2c6d2a2c8ef2b8a7040d74b84236d04
 THINGINO_RAPTOR_COMMON_SITE = https://github.com/gtxaspec/raptor-common
 THINGINO_RAPTOR_COMMON_SITE_METHOD = git
 THINGINO_RAPTOR_COMMON_INSTALL_STAGING = YES
@@ -11,7 +11,7 @@ endef
 define THINGINO_RAPTOR_COMMON_INSTALL_STAGING_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/librss_common.so \
 		$(STAGING_DIR)/usr/lib/librss_common.so
-	for h in rss_common.h rss_net.h rss_http.h rss_tls.h rss_ts.h rss_sei.h rss_sign.h rss_jpeg.h cJSON.h; do \
+	for h in rss_common.h rss_net.h rss_http.h rss_tls.h rss_ts.h rss_sei.h rss_sign.h rss_jpeg.h rss_aac.h cJSON.h; do \
 		$(INSTALL) -D -m 0644 $(@D)/include/$$h \
 			$(STAGING_DIR)/usr/include/$$h; \
 	done
