f12228b3483d726a0b3b24b935d4678f1f8b7f2f
Paul Philippov <paul@themactep.com>
2026-07-26 23:06:31 -0400

    package/prudynt-t: update to a03923a
    Update prudynt-t from 4518d5f to a03923a
    
    Hash change: 4518d5f543f445949de2aa99e9fe1f2488b96690 -> a03923a4bc1c916d8faf536c207fdfb2c7a39a3c
    
    Changelog:
    
      5eb35fd: cleanup: remove dead/misleading config and scripts
      a03923a: fix: resolve compiler warnings

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 4518d5f543f445949de2aa99e9fe1f2488b96690
+PRUDYNT_T_VERSION = a03923a4bc1c916d8faf536c207fdfb2c7a39a3c
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL_THINGINO_PATH)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt.json
 
