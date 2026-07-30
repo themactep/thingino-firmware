cf4baf86739c59e7f6c81d3fd995ccb25d4b9867
Paul Philippov <paul@themactep.com>
2026-07-30 15:00:36 -0400

    package/prudynt-t: bump to 9cd9cbe (OSD font + burn-in colors + docs)
    Signed-off-by: Paul Philippov <paul@themactep.com>

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = d81e6e93b1b2b9d906a7bb19c840fa13e403a0f9
+PRUDYNT_T_VERSION = 9cd9cbed32488defadecf82c846be285c1909245
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL_THINGINO_PATH)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt.json
 
