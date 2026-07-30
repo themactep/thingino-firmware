f9069fff3b1005319bcc72737c19f029f30cd540
Paul Philippov <paul@themactep.com>
2026-07-30 01:20:45 -0400

    prudynt-t: bump to 31975f5 (fix stale RTCP SR state on reconnect)
    Signed-off-by: Paul Philippov <paul@themactep.com>

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 39cebf3ef93c9f049b0b8f56f4d3011038888248
+PRUDYNT_T_VERSION = 31975f5f79623efab1175132de8caa70762c5be8
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL_THINGINO_PATH)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt.json
 
