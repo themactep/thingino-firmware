0598b996ffe4671371a223e2b37066d55796e678
Paul Philippov <paul@themactep.com>
2026-07-30 16:44:28 -0400

    package/prudynt-t: bump to 0f3fa5e (@ glyph for beats)
    Signed-off-by: Paul Philippov <paul@themactep.com>

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = 980f01e73d1b1e66b8549148af9a954532b29908
+PRUDYNT_T_VERSION = 0f3fa5edbcafd1513afce6f34430edea2a9cc18b
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL_THINGINO_PATH)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt.json
 
