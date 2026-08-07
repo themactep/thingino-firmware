30519f4c3333d3a5d98e95ce7d783e632056200d
gtxaspec <gtxent@gmail.com>
2026-08-07 15:46:22 -0700

    package/thingino-sensor-info: enable lto

--- package/thingino-sensor-info/thingino-sensor-info.mk
+++ package/thingino-sensor-info/thingino-sensor-info.mk
@@ -11,8 +11,8 @@ define THINGINO_SENSOR_INFO_BUILD_CMDS
 		CROSS_COMPILE=$(TARGET_CROSS) \
 		CFLAGS="$(TARGET_CFLAGS) -std=gnu99 -ffunction-sections -fdata-sections \
 			-fno-asynchronous-unwind-tables -fmerge-all-constants -fno-ident \
-			-fno-pie" \
-		LDFLAGS="$(TARGET_LDFLAGS) -Wl,--gc-sections -no-pie" \
+			-fno-pie -flto" \
+		LDFLAGS="$(TARGET_LDFLAGS) -Wl,--gc-sections -no-pie -flto" \
 		-C $(@D) sinfo
 endef
 
