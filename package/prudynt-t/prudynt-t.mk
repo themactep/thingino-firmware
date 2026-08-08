6030a750d241297095b30b8f502e7c5a2b84eb08
Lu-Fi <lutz@fiebach.de>
2026-08-08 19:07:33 +0200

    sync: forward accumulated upstream/master changes into our master
    These already landed on timps-package via routine "Merge upstream/master
    into timps-package" merges but were never forwarded to our own master
    (only package/timps content gets its own PR each cycle). Confirmed
    timps-package's upstream merge point is exactly upstream/master's
    current tip (bbd8a54a1) - nothing new to pull from upstream itself,
    this is purely catching our master up to what timps-package already has.
    
    - CI: firmware-master/-stable/prune-releases workflow tweaks
    - ~15 package version bumps (buildscope, faac, libflac, libhelix-aac/mp3,
      lightnvr, llhttp, nino, open-tx-isp, prudynt-t, scriba, thingino-dfu,
      thingino-onvif, thingino-raptor(-ipc), thingino-sensor-info,
      thingino-sounds, thingino-uhttpd)
    - new dropbear patch: disable wtmp/wtmpx/lastlog logging
    - thingino-diag: redact SSID and encryption key from WIFI-INFO output
    - thingino-onvif: disable local GetProfiles patch, superseded upstream

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = ac02b8014ab64d8ff3c3f8b49a9af0586ee7ad9e
+PRUDYNT_T_VERSION = 585aac4db0520dc9b1f78f15b6c4f7a3ecd9a9cd
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL_THINGINO_PATH)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt.json
 
