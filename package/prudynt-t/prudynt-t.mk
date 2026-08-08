c173a2a81ad01a060ae4cd8e75c5d721cf4fc471
Paul Philippov <paul@themactep.com>
2026-08-08 15:48:04 -0400

    package/prudynt-t: bump to d5336b3 (refuse corrupted config)
    Signed-off-by: Paul Philippov <paul@themactep.com>

--- package/prudynt-t/prudynt-t.mk
+++ package/prudynt-t/prudynt-t.mk
@@ -2,7 +2,7 @@ PRUDYNT_T_SITE_METHOD = git
 # PRUDYNT_T_SITE = https://github.com/gtxaspec/prudynt-t
 PRUDYNT_T_SITE = https://github.com/themactep/prudynt-t
 PRUDYNT_T_SITE_BRANCH = stable
-PRUDYNT_T_VERSION = c66bd5e0333570ca455e5a21d0d304be8dcb47f4
+PRUDYNT_T_VERSION = d5336b3d77f5c20e2c7add9b5aa2c0c435cd91e0
 
 PRUDYNT_T_OVERRIDE_FILE = $(BR2_EXTERNAL_THINGINO_PATH)/$(CAMERA_SUBDIR)/$(CAMERA)/prudynt.json
 
