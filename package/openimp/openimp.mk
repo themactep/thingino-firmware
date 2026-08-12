f45fb4efd789fb4d626bb5b6bff4b871d76d34f6
Matt Davis <matteius@gmail.com>
2026-08-11 20:54:58 -0400

    openisp: pin merged OpenTX and OpenIMP heads

--- package/openimp/openimp.mk
+++ package/openimp/openimp.mk
@@ -7,7 +7,7 @@
 OPENIMP_SITE_METHOD = git
 OPENIMP_SITE = https://github.com/opensensor/openimp
 OPENIMP_SITE_BRANCH = main
-OPENIMP_VERSION = 759d573c93cfb77926c8f584e12b8156544dc8d4
+OPENIMP_VERSION = 614a6fbab89752c2317a2801e78b7c3f66e54b2f
 
 # Upstream describes OpenIMP as MIT but does not currently ship a top-level
 # license file for legal-info to collect.
