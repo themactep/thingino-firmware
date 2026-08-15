5c97d34e78edaa4c615a042e68de3dd70e5b6600
Matt Davis <matteius@gmail.com>
2026-08-15 06:19:35 -0400

    openimp: pin corrected T31 AE luma query

--- package/openimp/openimp.mk
+++ package/openimp/openimp.mk
@@ -7,7 +7,7 @@
 OPENIMP_SITE_METHOD = git
 OPENIMP_SITE = https://github.com/opensensor/openimp
 OPENIMP_SITE_BRANCH = main
-OPENIMP_VERSION = 614a6fbab89752c2317a2801e78b7c3f66e54b2f
+OPENIMP_VERSION = 7c6ca718170a01a86c6222a3af21b662efa6bbd9
 
 # Upstream describes OpenIMP as MIT but does not currently ship a top-level
 # license file for legal-info to collect.
