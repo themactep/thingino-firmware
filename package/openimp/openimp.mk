4cd0d29ea3cbd450efb7df1e16732092190265cd
Paul Philippov <paul@themactep.com>
2026-08-16 17:41:42 -0400

    package/openimp: update to 3f04d8e
    Update openimp from 759d573 to 3f04d8e
    
    Hash change: 759d573c93cfb77926c8f584e12b8156544dc8d4 -> 3f04d8e756b31cd16303f828de69396c1a5acfe3
    
    Changelog:
    
      6ed4158: t40: correct audio stream and volume ABI
      614a6fb: t41: adapt security tuning from scene statistics
      7c6ca71: t31: query the OEM AE luma control
      81fe2eb: fix(t31): prevent encoder polling starvation
      3f04d8e: fix(t31): preserve configured encoder cadence

--- package/openimp/openimp.mk
+++ package/openimp/openimp.mk
@@ -7,7 +7,7 @@
 OPENIMP_SITE_METHOD = git
 OPENIMP_SITE = https://github.com/opensensor/openimp
 OPENIMP_SITE_BRANCH = main
-OPENIMP_VERSION = 759d573c93cfb77926c8f584e12b8156544dc8d4
+OPENIMP_VERSION = 3f04d8e756b31cd16303f828de69396c1a5acfe3
 
 # Upstream describes OpenIMP as MIT but does not currently ship a top-level
 # license file for legal-info to collect.
