8c7a7487c8df8881d4a7ba2fd9c41c270aed72fc
Paul Philippov <paul@themactep.com>
2026-08-17 00:07:41 -0400

    package/thingino-onvif: update to 70d35cf, add optional Synology compat flag
    Bump to 70d35cf which gates the non-compliant Synology shims (synthetic
    CreateProfile, deletable SynoProfileToken) behind
    HAVE_SYNOLOGY_COMPAT. Add the BR2_PACKAGE_THINGINO_ONVIF_SYNOLOGY_COMPAT
    option; default builds are spec-compliant, the shims are only compiled
    and installed when explicitly opted in.
    
    Signed-off-by: Paul Philippov <paul@themactep.com>

--- package/thingino-onvif/thingino-onvif.mk
+++ package/thingino-onvif/thingino-onvif.mk
@@ -1,12 +1,16 @@
 THINGINO_ONVIF_SITE_METHOD = git
 THINGINO_ONVIF_SITE = https://github.com/themactep/thingino-onvif
 THINGINO_ONVIF_SITE_BRANCH = master
-THINGINO_ONVIF_VERSION = 74b4f702320964a066f4de25f29801ee2c958d1a
+THINGINO_ONVIF_VERSION = 70d35cf2d41e499ed8ba539636bcd717ac0f36cd
 
 THINGINO_ONVIF_LICENSE = MIT
 THINGINO_ONVIF_LICENSE_FILES = LICENSE
 
 THINGINO_ONVIF_DEPENDENCIES += thingino-jct thingino-mxml
+
+ifeq ($(BR2_PACKAGE_THINGINO_ONVIF_SYNOLOGY_COMPAT),y)
+MAKE_OPTS += HAVE_SYNOLOGY_COMPAT=y
+endif
 
 ifeq ($(BR2_PACKAGE_MBEDTLS),y)
 THINGINO_ONVIF_DEPENDENCIES += mbedtls
@@ -54,6 +58,7 @@ define THINGINO_ONVIF_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/media_service_files \
 		$(@D)/res/media_service_files/*
 
+
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/var/www/onvif/media2_service_files
 	$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/media2_service_files \
 		$(@D)/res/media2_service_files/*
@@ -94,6 +99,13 @@ define THINGINO_ONVIF_INSTALL_TARGET_CMDS
 	# S96onvif_discovery is streamer-specific and installed by the selected streamer package.
 	$(INSTALL) -D -m 0755 $(THINGINO_ONVIF_PKGDIR)/files/S97onvif_notify \
 		$(TARGET_DIR)/etc/init.d/S97onvif_notify
+	# Synology compatibility shims are optional and non-compliant; only install
+	# their templates when the build opted in (BR2_PACKAGE_THINGINO_ONVIF_SYNOLOGY_COMPAT).
+	if [ "$(BR2_PACKAGE_THINGINO_ONVIF_SYNOLOGY_COMPAT)" = "y" ]; then \
+		$(INSTALL) -m 0644 -t $(TARGET_DIR)/var/www/onvif/media_service_files \
+			$(@D)/res/synology/CreateProfile.xml \
+			$(@D)/res/synology/DeleteProfile.xml; \
+	fi
 endef
 
 $(eval $(generic-package))
