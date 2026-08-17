95d840d095e80b9afea14b7d79a3d456b8a07fd4
Paul Philippov <paul@themactep.com>
2026-08-16 20:43:19 -0400

    thingino-odhcp6c: bump

--- package/thingino-odhcp6c/thingino-odhcp6c.mk
+++ package/thingino-odhcp6c/thingino-odhcp6c.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-THINGINO_ODHCP6C_VERSION = daf4ec3054e753c99fdcc3ac5464926548b38351
+THINGINO_ODHCP6C_VERSION = 10a52220aec9d45803518d8cc4d63e552484ed61
 THINGINO_ODHCP6C_SITE = https://git.openwrt.org/project/odhcp6c.git
 THINGINO_ODHCP6C_SITE_METHOD = git
 THINGINO_ODHCP6C_LICENSE = GPL-2.0
