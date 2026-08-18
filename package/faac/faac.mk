d8296fa330f1f89ecb0e37a151fcc636e325b507
Paul Philippov <paul@themactep.com>
2026-08-18 15:29:36 -0400

    package/faac: pin to release 2.1

--- package/faac/faac.mk
+++ package/faac/faac.mk
@@ -1,19 +1,21 @@
-FAAC_SITE_METHOD = git
-FAAC_SITE = https://github.com/knik0/faac
-FAAC_SITE_BRANCH = master
-FAAC_VERSION = 9392e861480c9845a92e7591349b4f9a90a65c1a
+FAAC_VERSION = 2.1
+FAAC_SITE = $(call github,knik0,faac,faac-$(FAAC_VERSION))
 
 FAAC_LICENSE = MPEG-4-Reference-Code, LGPL-2.1+
 FAAC_LICENSE_FILES = COPYING
 
 FAAC_INSTALL_STAGING = YES
+FAAC_INSTALL_TARGET = YES
 
-# Upstream builds with meson and generates config.h, faac.pc and the
-# versioned shared lib (libfaac.so.1) itself. The CLI frontend is only
-# built and installed when BR2_PACKAGE_FAAC_INSTALL_BIN is enabled.
+FAAC_CFLAGS = $(TARGET_CFLAGS) -ffast-math
+
 FAAC_CONF_OPTS = \
-	-Dfrontend=$(if $(BR2_PACKAGE_FAAC_INSTALL_BIN),true,false) \
-	-Dmax-channels=2 \
-	-Dsbr-decimation=4
+	-Dmax-channels=2
+
+ifeq ($(BR2_PACKAGE_FAAC_INSTALL_BIN),y)
+FAAC_CONF_OPTS += -Dfrontend=true
+else
+FAAC_CONF_OPTS += -Dfrontend=false
+endif
 
 $(eval $(meson-package))
