789596008acfb722dc0574ae4c934cfd057c40d3
Paul Philippov <paul@themactep.com>
2026-08-24 20:49:58 -0400

    package/open-tx-isp: update to 99b1141
    Update open-tx-isp from 913e481 to 99b1141
    
    Hash change: 913e4812a4181ac256b36227e9b33f506a3dc6bd -> 99b11416dcfac34a0a2f073836202ffa514d7c91
    
    Changelog:
    
      1f0ce5d: check pt
      a5dc17a: t30: add recovered ISP driver baseline
      dde6453: t30: share sensor and subdevice policies
      c0adc12: refactor(t30): share SDK math and frame ABI
      99b1141: refactor(t30): restore OEM state and ISP allocator

--- package/open-tx-isp/open-tx-isp.mk
+++ package/open-tx-isp/open-tx-isp.mk
@@ -7,7 +7,7 @@
 OPEN_TX_ISP_SITE_METHOD = git
 OPEN_TX_ISP_SITE = https://github.com/opensensor/open-tx-isp
 OPEN_TX_ISP_SITE_BRANCH = main
-OPEN_TX_ISP_VERSION = 913e4812a4181ac256b36227e9b33f506a3dc6bd
+OPEN_TX_ISP_VERSION = 99b11416dcfac34a0a2f073836202ffa514d7c91
 
 # Upstream identifies the project as GPLv3 but does not currently ship a
 # top-level license file for legal-info to collect.
