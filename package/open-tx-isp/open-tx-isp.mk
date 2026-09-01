f6875aa7852fb3b905b8cfb91fe5faa96d25b4f5
Matt Davis <matteius@gmail.com>
2026-09-01 04:22:46 -0400

    package/open-tx-isp: update to 13fd247
    Update open-tx-isp from 822b137 to 13fd247
    
    Hash change: 822b137cae2a1501ec58065088f00b151d367b21 -> 13fd24755114a1b7a2e09dc75236eb08f6185029
    
    Changelog:
    
      c323b04: t40: restore calibrated image pipeline controls
      13fd247: t4: require V4L2 DMA buffer providers

--- package/open-tx-isp/open-tx-isp.mk
+++ package/open-tx-isp/open-tx-isp.mk
@@ -7,7 +7,7 @@
 OPEN_TX_ISP_SITE_METHOD = git
 OPEN_TX_ISP_SITE = https://github.com/opensensor/open-tx-isp
 OPEN_TX_ISP_SITE_BRANCH = main
-OPEN_TX_ISP_VERSION = 822b137cae2a1501ec58065088f00b151d367b21
+OPEN_TX_ISP_VERSION = 13fd24755114a1b7a2e09dc75236eb08f6185029
 
 # Upstream identifies the project as GPLv3 but does not currently ship a
 # top-level license file for legal-info to collect.
