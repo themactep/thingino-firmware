e10d1b55efd20184fad7ae6046fd5291b555658f
Paul Philippov <paul@themactep.com>
2026-09-14 14:49:30 -0400

    package/nino: update to v0.2.0
    Update nino from v0.1.0 to v0.2.0
    
    Hash change: v0.1.0 -> v0.2.0
    
    (No changelog available)

--- package/nino/nino.mk
+++ package/nino/nino.mk
@@ -2,7 +2,7 @@
 # nino
 ################################################################################
 
-NINO_VERSION = 0.1.0
+NINO_VERSION = 0.2.0
 NINO_SITE = $(call github,evanlin96069,nino,v$(NINO_VERSION))
 
 NINO_LICENSE = BSD-2-Clause
